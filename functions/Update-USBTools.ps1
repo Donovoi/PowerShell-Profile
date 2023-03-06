5304c6372b9ac170cde99732dd9571cf26831d44
Michael Moran <michael@theablecoder.io>
2023-03-07 03:07:46 +1100

    deprecated dotnetinstall

--- functions/Update-USBTools.ps1
+++ functions/Update-USBTools.ps1
@@ -14,7 +14,7 @@ function Update-USBTools {
     Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-VcRedist -verbose"'
     Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-VSCodes -verbose"'
     Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Get-Zimmer -verbose"'
-    Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Install-LatestDotNet -verbose"'
+    Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget install Microsoft.DotNet.SDK.Preview"'
     Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Git-Pull -verbose"'
     Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-PowerShell -verbose"'
 cargo install cargo-update
