52403742419f0df8c0020865c52cdc12f8862f8b
Michael Moran <michael@theablecoder.io>
2023-03-13 17:12:27 +1100

    fix vscodes

--- functions/Update-VScodes.ps1
+++ functions/Update-VScodes.ps1
@@ -4,10 +4,12 @@ function Update-VScodes {
 		
 	)	
 	process {
+		$Global:XWAYSUSB = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter
+
 		Invoke-WebRequest 'https://code.visualstudio.com/sha/download?build=insider&os=win32-x64-archive' -OutFile "$ENV:USERPROFILE\downloads\insiders.zip" -Verbose
-		Expand-Archive "$ENV:USERPROFILE\Downloads\insiders.zip" -DestinationPath $XWAYSUSB + "\vscode-insider\" -Force -Verbose
+		Expand-Archive "$ENV:USERPROFILE\Downloads\insiders.zip" -DestinationPath $("$XWAYSUSB" + "\vscode-insider\") -Force -Verbose
 		Invoke-WebRequest 'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive' -OutFile "$ENV:USERPROFILE\downloads\vscode.zip" -Verbose
-		Expand-Archive "$ENV:USERPROFILE\Downloads\vscode.zip" -DestinationPath $XWAYSUSB + "\vscode\" -Force -Verbose
+		Expand-Archive "$ENV:USERPROFILE\Downloads\vscode.zip" -DestinationPath $("$XWAYSUSB" + "\vscode\") -Force -Verbose
 	}
 }
    
