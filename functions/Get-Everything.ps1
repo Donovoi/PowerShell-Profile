dd9f91c0b9d034794d8b1f995b2120bf92c3e556
Michael Moran <michael@theablecoder.io>
2023-03-16 17:21:09 +1100

    added everything search cmdlet - WIP

---
+++ functions/Get-Everything.ps1
@@ -0,0 +1,38 @@
+<#
+.SYNOPSIS
+    This is a PowerShell Cmdlet that uses voidtools' Everything to get a list of all files in a folder and its subfolders.
+.DESCRIPTION
+    Either given a directory, or given a query using Everything syntax, this function will return a list of all files in a folder and its subfolders.
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    Specify a URI to a help page, this will show when Get-Help -Online is used.
+.EXAMPLE
+    Test-MyTestFunction -Verbose
+    Explanation of the function or its result. You can include multiple examples with additional .EXAMPLE lines
+#>
+
+
+
+function Get-Everything {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        Start-Process -FilePath  ".\Non PowerShell Tools\everything portable\es.exe" -ArgumentList "-full-path-and-name -export-csv OUTPUT.csv folder: D:\ -no-header" -Wait -NoNewWindow
+
+
+        [string[]]$FilePathsArray = Get-Content .\OUTPUT.csv -ReadCount 0
+    
+    }
+    
+    end {
+        
+    }
+}
--- functions/Set-ACLs.ps1
+++ functions/Set-ACLs.ps1
@@ -2,40 +2,27 @@ function Set-ACLs {
     [CmdletBinding()]
     param (
        
-         [Parameter(Mandatory = $false)]
-         [string]
-        $FolderToScan = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter + "\"
+        [Parameter(Mandatory = $false)]
+        [string]
+        $FolderToScan = $XWAYSUSB + "\"
 
     )
+    $PWD
     $ErrorActionPreference = 'Continue'
     $NewOwner = New-Object System.Security.Principal.NTAccount("$ENV:COMPUTERNAME", "$ENV:USERNAME")
-    # Now we run the fastfileenum exe to get a list of files appeneded to a text file on the desktop
-    Start-Process -FilePath "D:\Projects\PowerShell-Profile\Non PowerShell Tools\FastFileEnum\bin\Debug\net7.0\win32.exe" -ArgumentList "$(Resolve-Path $FolderToScan)" -Wait -NoNewWindow -Verbose
-    
-    # read the large text file line by line into an in memory collection. this needs to be the fastest way to do this
-    $RAWPaths = Import-Content -Path $env:USERPROFILE\desktop\files.txt -Verbose
-    
+    # Now we run the es.exe tool to get a list of all files in the $FolderToScan
+    # Start-Process -FilePath "D:\Projects\PowerShell-Profile\Non PowerShell Tools\FastFileEnum\bin\Debug\net7.0\win32.exe" -ArgumentList "$(Resolve-Path $FolderToScan)" -Wait -NoNewWindow -Verbose
 
-    if (-not ([string]::IsNullOrWhiteSpace($RAWPaths))) {
-        [string[]]$Files = if (-not ([string]::IsNullOrWhiteSpace($RAWPaths))) {
-          $RAWPaths.Split()
-        }
-        if (-Not ([string]::IsNullOrWhiteSpace($Files))) {
-          $FastFileList = [System.Collections.Generic.HashSet[string]]::new([string[]]($Files), [System.StringComparer]::OrdinalIgnoreCase)
-        }
-    }
+
     [GC]::Collect()
 
     # Get the total number of files
-    $totalFiles = $FastFileList.Count
+    $totalFiles = $FilePathsArray.Count
     $filesdone = 0
 
-    $files | ForEach-Object -Process {
-        $f = Resolve-Path $_
+    $FilePathsArray | ForEach-Object -Process {
+        $f = Resolve-Path -LiteralPath $_
         $acl = (Get-Acl $f)
-        # $fObject = $f | Select-Object @{Name = 'FullName'; Expression = { $_ } }
-        # $aclObject = $acl | Select-Object @{Name = 'Acl'; Expression = { $_ } }
-        # Select-Object $fObject, $aclObject | Export-Csv -Path $env:USERPROFILE\desktop\acls.csv -Append -Force
         If ($acl.Owner -ne $NewOwner) {
             $acl.SetOwner($NewOwner)
             Set-Acl -Path $f -AclObject $acl
@@ -50,4 +37,4 @@ function Set-ACLs {
 
     }
 }
-# Set-ACLs -Verbose
+#Set-ACLs -Verbose
