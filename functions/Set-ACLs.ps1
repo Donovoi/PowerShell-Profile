bf0e7801cf6faf8a3912c9fe8cef592a4a58d943
Michael Moran <michael@theablecoder.io>
2023-03-23 18:58:49 +1100

    almost complete

--- functions/Set-ACLs.ps1
+++ functions/Set-ACLs.ps1
@@ -13,28 +13,44 @@ function Set-ACLs {
     # Now we run the es.exe tool to get a list of all files in the $FolderToScan
     # Start-Process -FilePath "D:\Projects\PowerShell-Profile\Non PowerShell Tools\FastFileEnum\bin\Debug\net7.0\win32.exe" -ArgumentList "$(Resolve-Path $FolderToScan)" -Wait -NoNewWindow -Verbose
 
+    # TODO: Replace this with the Get-Everything cmdlet
+    Start-Process -FilePath  ".\Non PowerShell Tools\everything portable\es.exe" -ArgumentList "-full-path-and-name -export-csv OUTPUT.csv folder: $($XWAYSUSB)\ -no-header" -Wait -NoNewWindow
 
-    [GC]::Collect()
+    $OutPutCSV = Resolve-Path .\OUTPUT.csv
 
-    # Get the total number of files
-    $totalFiles = $FilePathsArray.Count
-    $filesdone = 0
-
-    $FilePathsArray | ForEach-Object -Process {
-        $f = Resolve-Path -LiteralPath $_
-        $acl = (Get-Acl $f)
+    Get-Content $outputcsv -ReadCount 1 | ForEach-Object -Process {
+        
+        # handle apostrophes in file paths
+        $SanitizedFilePath = $_.Trim('"')
+        $ResolvedFilePath = Resolve-Path -Path $SanitizedFilePath -Verbose
+        $acl = (Get-Acl $ResolvedFilePath)
         If ($acl.Owner -ne $NewOwner) {
             $acl.SetOwner($NewOwner)
-            Set-Acl -Path $f -AclObject $acl
-
+            Set-Acl -Path $ResolvedFilePath -AclObject $acl
+            [GC]::Collect()
         }
-        $filesdone += 1
-        $progress = $filesdone / $totalFiles * 100
-        $progress = $progress.ToString('0.00')
-        Write-Progress -PercentComplete $progress -Completed $filesdone -SecondsRemaining $((($totalFiles - $filesdone) * ($progress / 100) * 60))
-
-
-
     }
+    
+
+    # # Get the total number of files
+    # $totalFiles = $FilePathsArray.Count
+    # $filesdone = 0
+
+    # $FilePathsArray | ForEach-Object -Process {
+    #     $f = Resolve-Path -LiteralPath $_
+    #     $acl = (Get-Acl $f)
+    #     If ($acl.Owner -ne $NewOwner) {
+    #         $acl.SetOwner($NewOwner)
+    #         Set-Acl -Path $f -AclObject $acl
+
+    #     }
+    #     $filesdone += 1
+    #     $progress = $filesdone / $totalFiles * 100
+    #     $progress = $progress.ToString('0.00')
+    #     Write-Progress -PercentComplete $progress -Completed $filesdone -SecondsRemaining $((($totalFiles - $filesdone) * ($progress / 100) * 60))
+
+
+
+    
 }
-#Set-ACLs -Verbose
+Set-ACLs -Verbose
