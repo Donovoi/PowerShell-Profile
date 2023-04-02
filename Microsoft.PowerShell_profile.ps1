1f57f12146a4ae255c5b14919feeaaea61ba1eab
Michael <Michael@theablecoder.io>
2023-04-02 14:27:22 +1000

    more info on importing module

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,6 +8,7 @@ $FunctionsFolder.ForEach{ .$_.FullName }
 $ModulesFolder = Get-ChildItem -Path "$profileparentpath/Modules/" -Include "*.ps*" -Recurse
 
 $ModulesFolder.ForEach{
+  Out-Host "Importing module: $($_.FullName)"
   Import-Module -Name $_.FullName
 }
 
