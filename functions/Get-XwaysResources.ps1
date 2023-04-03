a6596210773e5a3f6e33f53a864e5f8f4bd4265e
michael <michael@theablecoder.io>
2023-04-03 16:31:58 +1000

    add templates - not tested

--- functions/Get-XwaysResources.ps1
+++ functions/Get-XwaysResources.ps1
@@ -2,7 +2,11 @@
 .SYNOPSIS
     Download the extra resources needed for xways forensics at the moment this is just Excire and Conditional Coloring
 .DESCRIPTION
-    Long description
+    Download the extra resources needed for xways forensics at the moment this is just Excire and Conditional Coloring
+.PARAMETER DestinationFolder
+    The folder to download the resources to. I have set this to my preferred location of $XWAYSUSB\xwfportable (where $XWAYSUSB is the drive letter of my Xways USB and is a GLOBAL variable in my profile)
+.PARAMETER Credentials
+    The credentials to use to download the resources
 .EXAMPLE
     Get-XwaysResources -DestinationFolder "C:\Xways" -Credentials $(Get-Credential)
 #>
@@ -13,15 +17,20 @@ function Get-XwaysResources {
         [Parameter()]
         [string]
         $DestinationFolder = "$XWAYSUSB\xwfportable",
+        [Parameter()]
+        [string]
+        $XWScriptsAndTemplatesFolder = $(Resolve-PAth -Path "$XWAYSUSB\xwfportable\..\XWScriptsAndTemplates"),
+        
         [Parameter(Mandatory = $true)]
         [pscredential]
         $Credentials = $(Get-Credential)
     )
     
-    #   First Resource to grab is Excire
+    #   First we need to get the Username and Password from the credentials object
     $UsernamePlainText = $Credentials.GetNetworkCredential().UserName
     $PasswordPlainText = $Credentials.GetNetworkCredential().Password
 
+    #  Then we need to convert the username and password to base64 for basic http authentication
     $AuthenticationPair = "$($UsernamePlainText)`:$($PasswordPlainText)"
     $Bytes = [System.Text.Encoding]::UTF8.GetBytes($authenticationPair)
     $Base64AuthString = [System.Convert]::ToBase64String($bytes)
@@ -48,12 +57,12 @@ function Get-XwaysResources {
     $headers.Add("Referer", "https://x-ways.net/res/")
     $headers.Add("Accept-Encoding", "gzip, deflate")
     $headers.Add("Accept-Language", "en-US,en;q=0.9")
-    $response = (Invoke-WebRequest -Method $method -Uri $URI -MaximumRedirection $maximumRedirection -Headers $headers -UserAgent $userAgent -OutFile "Excire.zip")
+    $response = (Invoke-WebRequest -Method $method -Uri $URI -MaximumRedirection $maximumRedirection -Headers $headers -UserAgent $userAgent -OutFile "$ENV:TEMP\Excire.zip")
     $response
 
     # Extract zip to destination folder in the Excire folder
     Out-Host -InputObject "Extracting Excire.zip to $DestinationFolder\Excire"
-    Expand-Archive -Path "Excire.zip" -DestinationPath "$DestinationFolder\Excire" -Force
+    Expand-Archive -Path "$ENV:TEMP\Excire.zip" -DestinationPath "$DestinationFolder\Excire" -Force
 
     # headers for xways website - can possibly remove some of these
     $method = [Microsoft.PowerShell.Commands.WebRequestMethod]::"GET"
@@ -75,13 +84,36 @@ function Get-XwaysResources {
     $headers.Add("Referer", "https://x-ways.net/res/")
     $headers.Add("Accept-Encoding", "gzip, deflate")
     $headers.Add("Accept-Language", "en-US,en;q=0.9")
-    $response = (Invoke-WebRequest -Method $method -Uri $URI -MaximumRedirection $maximumRedirection -Headers $headers -UserAgent $userAgent -OutFile "Conditional Coloring.cfg")
+    $response = (Invoke-WebRequest -Method $method -Uri $URI -MaximumRedirection $maximumRedirection -Headers $headers -UserAgent $userAgent -OutFile "$ENV:TEMP\Conditional Coloring.cfg")
     $response
 
 
     # Copy Conditional Coloring.cfg to destination folder
     Out-Host -InputObject "Copying Conditional Coloring.cfg to $DestinationFolder"
-    Copy-Item -Path ".\Conditional Coloring.cfg" -Destination $DestinationFolder -Force
+    Copy-Item -Path "$ENV:TEMP\Conditional Coloring.cfg" -Destination $DestinationFolder -Force
+
+    # Now we copy all TPL files from x-ways.net/winhex/templates to the XWScriptsAndTemplates folder
+    # First we need to load the page and find all links that end in .tpl
+    # load the x-ways.net/winhex/templates page
+    $XWAYSTemplateNames = (Invoke-WebRequest -Uri "https://x-ways.net/winhex/templates/").Links.Where({ $_.href -like "*.tpl" }).href
+
+    # provide a count of how many templates were found
+    Out-Host -InputObject "There are $XWAYSTemplateNames.Count templates available"
+
+    # then download each template and save it to the XWScriptsAndTemplates folder
+    out-host -InputObject "Downloading templates to $XWScriptsAndTemplatesFolder"
+
+    $XWAYSTemplateNames.foreach{
+        Invoke-WebRequest -Uri $("https://x-ways.net/winhex/templates/$_") -OutFile "$DestinationFolder\XWScriptsAndTemplates\$_"
+    }
+
+
+
+
+
+
+
+    
 
 
 }
