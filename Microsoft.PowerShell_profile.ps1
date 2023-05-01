7af736f5c42660510dce2bdea776994d8aba610a
Michael Moran <michael@theablecoder.io>
2023-05-01 12:57:42 +1000

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -64,7 +64,7 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-if (-not ((Get-Command oh-my-posh).Source)) {
+if (-not (Get-Command oh-my-posh -erroraction silentlycontinue)) {
   Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 }
 RefreshEnv.cmd
