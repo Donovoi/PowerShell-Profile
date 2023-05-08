e7e27d66995a4bd47ae9a8a4ed51894dc41aa64c
michael <michael@theablecoder.io>
2023-05-08 12:32:27 +1000

    easy update powershell

--- functions/Update-PowerShell.ps1
+++ functions/Update-PowerShell.ps1
@@ -5,21 +5,23 @@ function Update-PowerShell {
   param(
 
   )
-  # First check the latest version of powershell
-  $LatestVersion = Invoke-WebRequest -Uri 'https://api.github.com/repos/PowerShell/PowerShell/releases/latest' -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty tag_name
-  # Check the currently installed version
-  $CurrentVersion = $PSVersionTable.psversion
-  # Compare the versions
-  if ($LatestVersion -gt $CurrentVersion) {
-    # If the latest version is newer than the current version, download the latest version
-    $DownloadURL = Invoke-WebRequest -Uri 'https://api.github.com/repos/PowerShell/PowerShell/releases/latest' -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty assets | Select-Object -ExpandProperty browser_download_url -First 1
-    $DownloadPath = "$env:TEMP\PowerShell-$LatestVersion-win-x64.msi"
-    Invoke-WebRequest -Uri $DownloadURL -OutFile $DownloadPath
-    # Install the latest version silently
-    Start-Process -FilePath msiexec.exe -ArgumentList "/i $DownloadPath /quiet /norestart" -Wait
-    # Remove the installer
-    Remove-Item $DownloadPath
-  }
-  Write-Output 'PowerShell is up to date'
+  # # First check the latest version of powershell
+  # $LatestVersion = Invoke-WebRequest -Uri 'https://api.github.com/repos/PowerShell/PowerShell/releases/latest' -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty tag_name
+  # # Check the currently installed version
+  # $CurrentVersion = $PSVersionTable.psversion
+  # # Compare the versions
+  # if ($LatestVersion -gt $CurrentVersion) {
+  #   # If the latest version is newer than the current version, download the latest version
+  #   $DownloadURL = Invoke-WebRequest -Uri 'https://api.github.com/repos/PowerShell/PowerShell/releases/latest' -UseBasicParsing | ConvertFrom-Json | Select-Object -ExpandProperty assets | Select-Object -ExpandProperty browser_download_url -First 1
+  #   $DownloadPath = "$env:TEMP\PowerShell-$LatestVersion-win-x64.msi"
+  #   Invoke-WebRequest -Uri $DownloadURL -OutFile $DownloadPath
+  #   # Install the latest version silently
+  #   Start-Process -FilePath msiexec.exe -ArgumentList "/i $DownloadPath /quiet /norestart" -Wait
+  #   # Remove the installer
+  #   Remove-Item $DownloadPath
+  # }
+  # Write-Output 'PowerShell is up to date'
+
+  winget install microsoft.powershell.preview
 }
 
