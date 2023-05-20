4f81b32fa74a68d26323796cc2b2934aaad1c55f
michael <michael@theablecoder.io>
2023-05-20 20:56:46 +1000

    test download different way

--- functions/Update-VScodes.ps1
+++ functions/Update-VScodes.ps1
@@ -1,16 +1,44 @@
 function Update-VScodes {
   [CmdletBinding()]
   param(
-
   )
   process {
-    Write-Host -Object "Script is running as $($MyInvocation.MyCommand.Name)" -Verbose
-    $Global:XWAYSUSB = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter
+      Write-Host -Object "Script is running as $($MyInvocation.MyCommand.Name)" -Verbose
+      $Global:XWAYSUSB = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter
 
-    Invoke-WebRequest 'https://code.visualstudio.com/sha/download?build=insider&os=win32-x64-archive' -OutFile "$ENV:USERPROFILE\downloads\insiders.zip" -Verbose
-    Expand-Archive "$ENV:USERPROFILE\Downloads\insiders.zip" -DestinationPath $("$XWAYSUSB" + "\vscode-insider\") -Force -Verbose
-    Invoke-WebRequest 'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive' -OutFile "$ENV:USERPROFILE\downloads\vscode.zip" -Verbose
-    Expand-Archive "$ENV:USERPROFILE\Downloads\vscode.zip" -DestinationPath $("$XWAYSUSB" + "\vscode\") -Force -Verbose
+      $Urls = @(
+          @{
+              URL             = 'https://code.visualstudio.com/sha/download?build=insider&os=win32-x64-archive'
+              OutFile         = "$ENV:USERPROFILE\downloads\insiders.zip"
+              DestinationPath = "$XWAYSUSB\vscode-insider\"
+          },
+          @{
+              URL             = 'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-archive'
+              OutFile         = "$ENV:USERPROFILE\downloads\vscode.zip"
+              DestinationPath = "$XWAYSUSB\vscode\"
+          }
+      )
+
+      function Download-UrlToFile {
+          [CmdletBinding()]
+          param([string]$url, [string]$outputPath)
+          $httpClient = New-Object System.Net.Http.HttpClient
+          $response = $httpClient.GetAsync($url).Result
+          $response.EnsureSuccessStatusCode()
+          [System.IO.File]::WriteAllBytes($outputPath, $response.Content.ReadAsByteArrayAsync().Result)
+      }
+
+      $funcDef = ${function:Download-UrlToFile}.ToString()
+
+      $Urls | ForEach-Object -Parallel {
+          ${function:Download-UrlToFile} = $using:funcDef
+          try {
+              Download-UrlToFile -Url $_.URL -OutputPath $_.OutFile
+              Write-Progress -Activity "Downloading and extracting" -Status $_.OutFile
+              Expand-Archive $_.OutFile -DestinationPath $_.DestinationPath -Force -Verbose
+          } catch {
+              Write-Error -Message "Error occurred: $($_.Exception.Message)" -ErrorAction Continue
+          }
+      }
   }
 }
-
