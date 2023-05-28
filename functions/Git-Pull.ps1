a6b691ded6ef99133c14dcbf2d1b0176da4d4069
Michael <Michael@theablecoder.io>
2023-05-28 15:30:22 +1000

    add message

--- functions/Git-Pull.ps1
+++ functions/Git-Pull.ps1
@@ -8,8 +8,14 @@ function Git-Pull {
 
   $ErrorActionPreference = 'Continue'
 
+  # Get all the repositories in the path specified. We are looking for directories that contain a .git directory
+  Write-Host "Searching for repositories in $Path, this can take a while..."
   $repositories = @{}
-  $repositories = Get-ChildItem -Path $Path -Recurse -Directory -Filter '.git' -Exclude "*google-ctf*" -Force | Split-Path -Parent
+  $repositories = Get-ChildItem -Path $Path -Recurse -Directory -Filter '.git' -Force | Split-Path -Parent
+
+  #Let the user know what we are doing and how many repositories we are working with
+  Write-Output "Found $($repositories.Count) repositories to pull from."
+
   # iterate through the hashtable and perform a git pull on each repository. THe repository is the parent of the .git directory
   #  We need to get the full path of the .git directory, then navigate to the parent directory and perform the git pull.
   $repositories.GetEnumerator() | ForEach-Object {
@@ -20,6 +26,8 @@ function Git-Pull {
     git config --global --add safe.directory $(Resolve-Path -Path $PWD)
     git pull --verbose
     Write-Output "git pull complete for $($_)"
+    #  Show progress
+    Write-Progress -Activity "Pulling from $($_)" -Status "Pulling from $($_)" -PercentComplete (($repositories.IndexOf($_) + 1) / $repositories.Count * 100)
   }
 
 
