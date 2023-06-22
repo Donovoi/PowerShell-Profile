a6290de5fed0e4531ee2289f7750a5eadbf7e9f7
michael <michael@theablecoder.io>
2023-06-23 09:07:39 +1000

    force refresh

--- functions/Update-PSProfile.ps1
+++ functions/Update-PSProfile.ps1
@@ -7,18 +7,18 @@ function Update-PSProfile {
   Write-Host -Object "Script is running as $($MyInvocation.MyCommand.Name)" -Verbose
   if (-not (Test-Path $PROFILE)) {
     New-Item $PROFILE -Force
-    $sourcefolder = $XWAYSUSB + "\Projects\Powershell-Profile\*"
+    $sourcefolder = $XWAYSUSB + '\Projects\Powershell-Profile\*'
     Copy-Item -Path $sourcefolder -Recurse -Container -Destination $parentpathprofile -Force
   }
   try {
     Set-Location -Path $parentpathprofile
-    git fetch --all;
-    $NAMEOFHEAD = $(git symbolic-ref refs/remotes/origin/HEAD);
-    git reset --hard origin/$($NAMEOFHEAD.split('/')[-1]);
+    git fetch --all
+    $NAMEOFHEAD = $(git symbolic-ref refs/remotes/origin/HEAD)
+    git reset --hard origin/$($NAMEOFHEAD.split('/')[-1])
 
   }
   catch {
-    $sourcefolder = $XWAYSUSB + "\Projects\Powershell-Profile\*"
+    $sourcefolder = $XWAYSUSB + '\Projects\Powershell-Profile\*'
     Copy-Item -Path $sourcefolder -Recurse -Container -Destination $parentpathprofile -Force
     Write-Error -Message "$_"
   }
@@ -29,7 +29,16 @@ function Update-PSProfile {
     $FunctionsFolder = Get-ChildItem -Path "$parentpathprofile/functions/*.ps*" -Recurse
     $FunctionsFolder.ForEach{ Import-Module $_.FullName }
     # Make sure chocolatey is correct path
-    $ENV:ChocolateyInstall = $XWAYSUSB + "\chocolatey apps\chocolatey\bin\"
+    $ENV:ChocolateyInstall = $XWAYSUSB + '\chocolatey apps\chocolatey\bin\'
+    # Exit to force refresh everything
+    # Get the current PowerShell process ID
+    $currentProcessId = $PID
+
+    # Start a new elevated instance of wt.exe
+    Start-Process wt.exe -Verb RunAs
+
+    # End the current PowerShell process
+    Stop-Process -Id $currentProcessId
 
   }
 }
