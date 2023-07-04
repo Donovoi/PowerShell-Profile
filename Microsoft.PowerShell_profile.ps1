ad395de3161840cdcc79c8e792e27824ceca8462
Michael <Michael@theablecoder.io>
2023-07-05 05:50:42 +1000

    delete f7 psreadline

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -124,55 +124,6 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-# This key handler shows the entire or filtered history using Out-GridView. The
-# typed text is used as the substring pattern for filtering. A selected command
-# is inserted to the command line without invoking. Multiple command selection
-# is supported, e.g. selected by Ctrl + Click.
-Set-PSReadLineKeyHandler -Key F7 `
-  -BriefDescription Get-History `
-  -LongDescription 'Show command history' `
-  -ScriptBlock {
-  $pattern = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
-  if ($pattern) {
-    $pattern = [regex]::Escape($pattern)
-  }
-
-  $history = [System.Collections.ArrayList]@(
-    $last = ''
-    $lines = ''
-    foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath)) {
-      if ($line.EndsWith('`')) {
-        $line = $line.Substring(0, $line.Length - 1)
-        $lines = if ($lines) {
-          "$lines`n$line"
-        }
-        else {
-          $line
-        }
-        continue
-      }
-
-      if ($lines) {
-        $line = "$lines`n$line"
-        $lines = ''
-      }
-
-      if (($line -cne $last) -and (!$pattern -or ($line -match $pattern))) {
-        $last = $line
-        $line
-      }
-    }
-  )
-  $history.Reverse()
-
-  $command = $history | Out-GridView -Title Get-History -PassThru
-  if ($command) {
-    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
-  }
-}
-
 
 # CaptureScreen is good for blog posts or email showing a transaction
 # of what you did when asking for help or demonstrating a technique.
