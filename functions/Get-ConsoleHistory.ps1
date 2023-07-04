4509cd885d57837b99d1a50e7e3ec05dd3885c3e
michael <michael@theablecoder.io>
2023-07-04 11:41:33 +1000

    fix

--- functions/Get-ConsoleHistory.ps1
+++ functions/Get-ConsoleHistory.ps1
@@ -5,8 +5,8 @@ function Get-ConsoleHistory {
         [switch]
         $update
     )
-    $ps1file = '.\F7History.ps1'
-    $jsonFile = '.\hashes.json'
+    $ps1file = $PROFILE | Split-Path | Join-Path -ChildPath 'functions' -AdditionalChildPath 'F7History.ps1'
+    $jsonFile = $PROFILE | Split-Path | Join-Path -ChildPath 'functions' -AdditionalChildPath 'hashes.json'
         
     if ($update) {       
         $oldHashes = New-Object PSObject -Property @{'Filename' = ''; 'MD5' = ''; 'SHA1' = '' }
@@ -22,8 +22,8 @@ function Get-ConsoleHistory {
         }
         if (-not(Test-Path $ps1file) -or $($oldHashes.MD5 -ne $md5HashResult.Hash) -or $($oldHashes.SHA1 -ne $sha1HashResult.Hash)) {
             Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/gui-cs/F7History/main/F7History.ps1' -OutFile $ps1file
-            if ($null -eq (Get-Content -Path $PROFILE -ErrorAction SilentlyContinue | Select-String -SimpleMatch ". $ps1file")) {
-                Add-Content -Path $PROFILE -Value ". $ps1file"
+            if ($null -eq (Get-Content -Path $PROFILE -ErrorAction SilentlyContinue | Select-String -SimpleMatch ". $($ps1file)")) {
+                Add-Content -Path $PROFILE -Value ". $($ps1file)"
                 . $PROFILE
             }
             $md5HashResult = Get-FileHash -Path $ps1file -Algorithm MD5
