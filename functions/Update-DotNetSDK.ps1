4bc186038ac9c44d507abe2492a0fec1b9d43b1e
Michael <Michael@theablecoder.io>
2023-07-16 08:43:16 +1000

    fix dotnet?

--- functions/Update-DotNetSDK.ps1
+++ functions/Update-DotNetSDK.ps1
@@ -1,14 +1,14 @@
 function Update-DotNetSDK {
     [CmdletBinding()]    
     param(
-        [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $false)]
         [string[]]
-        $DotNetVersions = @(3,5,6,7,"Preview")
+        $DotNetVersions = @('3_1', '5', '6', '7', 'Preview')
     )
-    foreach($version in $DotNetVersions) {
-        Write-Host "Installing .NET SDK version $version"
-        winget install "Microsoft.DotNet.SDK.$($version) --force --accept-source-agreements --accept-package-agreements"
-        Write-Host "Finished installing .NET SDK version $version"
+    foreach ($DotNetVersion in $DotNetVersions) {
+        Write-Host "Installing .NET SDK version $DotNetVersion"
+        winget install $('Microsoft.DotNet.SDK.' + $($DotNetVersion)) --force --accept-source-agreements --accept-package-agreements
+        Write-Host "Finished installing .NET SDK version $DotNetVersion"
     }
 
 }
