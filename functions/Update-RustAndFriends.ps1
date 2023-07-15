75e8947894fd90d0d386350dafa54a4b2c6466cc
Michael <Michael@theablecoder.io>
2023-07-16 08:50:38 +1000

    format

--- functions/Update-RustAndFriends.ps1
+++ functions/Update-RustAndFriends.ps1
@@ -5,21 +5,19 @@ function Update-RustAndFriends {
     param(
 
     )
-if (-not (Get-Command cargo -ErrorAction SilentlyContinue)) {
-    Write-Host "Installing Rust"
-    winget install Rustlang.Rust --force --accept-source-agreements --accept-package-agreements
-    Write-Host "Finished installing Rust"
-}
-else {
-    Write-Host "Updating Rust"
-    winget install Rustlang.Rust --force --accept-source-agreements --accept-package-agreements
-    Write-Host "Finished updating Rust"
-}
-Write-Host "Updating cargo"
-cargo install cargo-update
-cargo install-update -a
-Write-Host "Finished updating cargo"
-
-
+    if (-not (Get-Command cargo -ErrorAction SilentlyContinue)) {
+        Write-Host 'Installing Rust'
+        winget install Rustlang.Rust --force --accept-source-agreements --accept-package-agreements
+        Write-Host 'Finished installing Rust'
+    }
+    else {
+        Write-Host 'Updating Rust'
+        winget install Rustlang.Rust --force --accept-source-agreements --accept-package-agreements
+        Write-Host 'Finished updating Rust'
+    }
+    Write-Host 'Updating cargo'
+    cargo install cargo-update
+    cargo install-update -a
+    Write-Host 'Finished updating cargo'
 
 }
