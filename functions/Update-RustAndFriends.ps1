c3b052596269ad5da6f70cf12665b54ad8804292
Michael <Michael@theablecoder.io>
2023-07-16 08:54:56 +1000

    fix rust more

--- functions/Update-RustAndFriends.ps1
+++ functions/Update-RustAndFriends.ps1
@@ -5,16 +5,11 @@ function Update-RustAndFriends {
     param(
 
     )
-    if (-not (Get-Command cargo -ErrorAction SilentlyContinue)) {
-        Write-Host 'Installing Rust'
-        winget install Rustlang.Rustup --force --accept-source-agreements --accept-package-agreements
-        Write-Host 'Finished installing Rust'
-    }
-    else {
-        Write-Host 'Updating Rust'
-        winget install Rustlang.Rust --force --accept-source-agreements --accept-package-agreements
-        Write-Host 'Finished updating Rust'
-    }
+    
+    Write-Host 'Installing Rust'
+    winget install Rustlang.Rustup --force --accept-source-agreements --accept-package-agreements
+    Write-Host 'Finished installing Rust'
+
     Write-Host 'Updating cargo'
     cargo install cargo-update
     cargo install-update -a
