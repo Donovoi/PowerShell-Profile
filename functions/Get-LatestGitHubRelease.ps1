1162f7c256b252c5a75f2fa0d60aff8296a5f615
michael <michael@theablecoder.io>
2023-07-17 11:43:11 +1000

    update git hub release

--- functions/Get-LatestGitHubRelease.ps1
+++ functions/Get-LatestGitHubRelease.ps1
@@ -1,7 +1,7 @@
 function Get-LatestGitHubRelease {
   [OutputType([string])]
   [CmdletBinding()]
-  param (
+  param(
     [Parameter(Mandatory = $true)]
     [string]
     $OwnerRepository,
@@ -18,23 +18,44 @@ function Get-LatestGitHubRelease {
   )
 
   # Get the latest release from the GitHub API
-  $latestRelease = Invoke-RestMethod "https://api.github.com/repos/$OwnerRepository/releases/latest"
+  $apiurl = "https://api.github.com/repos/$OwnerRepository/releases"
+  $latestRelease = Invoke-RestMethod $($apiurl + '/latest') -ErrorAction SilentlyContinue 
+
+  $headers = @{
+    'Accept'               = 'application/vnd.github+json'
+    'X-GitHub-Api-Version' = '2022-11-28'
+  }
+
+  if (-not ($latestRelease)) {
+    Write-Log -Message "Looks like the repo doesn't have a latest tag, let's try another way" -Level Warning
+    # Get the asset with the specified name
+
+    $ManuallatestRelease = Invoke-RestMethod -Uri $apiurl -Headers $headers | Sort-Object -Property created_at | Select-Object -Last 1
+
+    $manualDownloadurl = $ManuallatestRelease.assets.Browser_Download_url | Select-Object -First 1
+
+  }
+
+  $releasetolookfor = $latestRelease.assets ? $latestRelease.assets : $($manualDownloadurl -split '\/')[-1]
 
   # Find the asset with the specified name
-  $asset = $latestRelease.assets | Where-Object { $_.name -like $AssetName }
+  $asset = $releasetolookfor | Where-Object { $_ -like "*$AssetName*" } | Select-Object -First 1
 
   # Download the asset
-  $DownloadPath = Join-Path -Path $DownloadPathDirectory -ChildPath $asset.name
+  $DownloadPath = Join-Path -Path $DownloadPathDirectory -ChildPath $($asset.Name ? $asset.Name : $asset)
+  Invoke-WebRequest $($asset.browser_download_url ? $asset.browser_download_url : $manualDownloadurl ) -OutFile $downloadPath
 
-  Invoke-WebRequest $asset.browser_download_url -OutFile $downloadPath
-  # Extract the asset if the switch is set
   if ($ExtractZip) {
+    #  check if the file is a zip file
+    if ($DownloadPath -notlike '*.zip') {
+      Write-Log -Message 'The downloaded file is not a zip file, skipping extraction' -Level Warning
+      return $DownloadPath
+    }
     Expand-Archive -Path $DownloadPath -DestinationPath $DownloadPathDirectory -Force
-    Write-Host "Extracted $DownloadPath to $DownloadPathDirectory"
+    Write-Log -Message "Extracted $DownloadPath to $DownloadPathDirectory"
   }
   else {
-    Write-Host "Downloaded $DownloadPath to $DownloadPathDirectory"
+    Write-Log -Message "Downloaded $DownloadPath to $DownloadPathDirectory"
   }
-  # Return the path to the downloaded asset
   return $DownloadPath
 }
