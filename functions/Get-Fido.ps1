f28ffacb54079ee30f04ae3b2b8d7caaa0a809e0
Your Name <you@example.com>
2023-08-10 13:40:45 +1000

    update fido

--- functions/Get-Fido.ps1
+++ functions/Get-Fido.ps1
@@ -1,12 +1,26 @@
 function Get-Fido {
   [CmdletBinding()]
   param()
-  
-  $scriptUrl = 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1'
-  
+
+  $scriptUrl = 'https://github.com/pbatard/Fido/releases/download/v1.50/Fido.ps1.lzma'
+
   try {
-    # Use Invoke-WebRequest to download the script and execute it directly
-    Invoke-Command -ScriptBlock ([Scriptblock]::Create((Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing | Select-Object -ExpandProperty Content)))
+    # Use Invoke-WebRequest to download the script
+    $scriptContent = Get-latestGithubRelease
+    
+    # Convert the script content to bytes using the UTF-8 encoding
+    $scriptBytes = [System.Text.Encoding]::UTF8.GetBytes($scriptContent)
+    
+    # Use the default encoding on your machine
+    $scriptBytesDefaultEncoding = [System.Text.Encoding]::Convert([System.Text.Encoding]::UTF8, [System.Text.Encoding]::Default, $scriptBytes)
+    
+    # Unpack the LZMA file (assuming you have the SevenZipSharp library installed)
+    Add-Type -Path 'C:\Path\To\SevenZipSharp.dll'
+    $sevenZip = New-Object SevenZip.SevenZipExtractor('C:\Path\To\LzmaFile.lzma')
+    $sevenZip.ExtractArchive('C:\Path\To\OutputDirectory')
+    
+    # Execute the script
+    Invoke-Expression -Command ([System.Text.Encoding]::Default.GetString($scriptBytesDefaultEncoding))
   }
   catch {
     Write-Error "Failed to download or execute the Fido script: $($_.Exception.Message)"
