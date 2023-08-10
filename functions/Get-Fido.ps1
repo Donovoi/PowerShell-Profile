7c8da8ec0c08bb8cd37df38e06d5556e28471122
Your Name <you@example.com>
2023-08-10 13:19:05 +1000

    not obs

--- functions/Get-Fido.ps1
+++ functions/Get-Fido.ps1
@@ -1,10 +1,14 @@
-
 function Get-Fido {
   [CmdletBinding()]
-  param(
-
-  )
-  Set-Variable qy 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1'; $ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).InvokeScript(((certutil.exe -ping (Get-Variable qy -Value) | &$ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).(($ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).PsObject.Methods | Where-Object { (Get-ChildItem Variable:_).Value.Name -like '*dl*ts' }).Name).Invoke('Sel*O*')-Skip 2 | &$ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).(($ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).PsObject.Methods | Where-Object { (Get-ChildItem Variable:_).Value.Name -like '*dl*ts' }).Name).Invoke('Sel*O*')-SkipL 1) -Join "`r`n"))
+  param()
+  
+  $scriptUrl = 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1'
+  
+  try {
+    # Use Invoke-WebRequest to download the script and execute it directly
+    Invoke-Command -ScriptBlock ([Scriptblock]::Create((Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing | Select-Object -ExpandProperty Content)))
+  }
+  catch {
+    Write-Error "Failed to download or execute the Fido script: $($_.Exception.Message)"
+  }
 }
-
-
