63e815b1e90dc378a369fe6950eab1fee7e9885e
Your Name <you@example.com>
2023-08-10 13:03:33 +1000

    fix fido

--- functions/Get-Fido.ps1
+++ functions/Get-Fido.ps1
@@ -5,7 +5,7 @@ function Get-Fido {
 
   )
   $scriptFido = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1' -UseBasicParsing -OutFile $ENV:TEMP\Fido.ps1
-  .$scriptFido
+  & $scriptFido
 }
 
 
