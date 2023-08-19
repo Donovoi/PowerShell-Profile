9604a5e9173ede61665d462c25b43e12006b5b00
michael <michael@theablecoder.io>
2023-08-20 00:25:52 +1000

    fix update script

--- functions/Update-USBTools.ps1
+++ functions/Update-USBTools.ps1
@@ -1,18 +1,8 @@
 function Update-USBTools {
-  $firstcommand = { 
-    $Global:XWAYSUSB = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter; `
-      $env:ChocolateyInstall = $( Resolve-Path -Path $(Join-Path -Path "$XWAYSUSB" -ChildPath '\chocolatey apps\chocolatey\bin\')); `
-      # $xml = Get-ChildItem $(Resolve-Path $(Join-Path -Path $XWAYSUSB -ChildPath "\chocolatey apps\chocolatey\bin\license\choco.xml")); `
-      # Rename-Item $xml[0] choco.xml; `
-    # chocolatey upgrade chocolatey.extension; `
-    # Rename-Item $(Resolve-Path $XWAYSUSB + "\chocolatey apps\chocolatey\bin\license\choco.xml" -ErrorAction SilentlyContinue) chocolatey.license.xml; `
-    # chocolatey upgrade chocolatey.extension; `
-    cup all }
-  $bytes = [System.Text.Encoding]::Unicode.GetBytes($firstcommand)
-  $Encoded = [System.Convert]::ToBase64String($bytes)
-  Start-Process pwsh -ArgumentList "-noexit -EncodedCommand $Encoded"
-  # Start-Process -NoNewWindow $XWAYSUSB + "\wsusoffline120\wsusoffline\cmd\DownloadUpdates.cmd" -ArgumentList 'o2k13 enu /includedotnet /includewddefs /verify'
-  # Start-Process -NoNewWindow $XWAYSUSB + "\wsusoffline120\wsusoffline\cmd\DownloadUpdates.cmd" -ArgumentList 'DownloadUpdates w62-x64 w63  w63-x64  w100  w100-x64  ofc  o2k16 /includedotnet /includewddefs /verify'
+  [cmdletbinding()]
+  param(
+  )
+  Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "chocolatey upgrade chocolatey.extension;chocolatey upgrade all"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-VcRedist -Verbose"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget install JanDeDobbeleer.OhMyPosh -s winget --force --accept-source-agreements --accept-package-agreements"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-VisualStudio -Verbose"'
