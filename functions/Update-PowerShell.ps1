1ff191cca1e6eef41dc0a3edfd1b370c022f7ed5
michael <michael@theablecoder.io>
2023-08-23 16:33:02 +1000

    update both powershells

--- functions/Update-PowerShell.ps1
+++ functions/Update-PowerShell.ps1
@@ -13,12 +13,42 @@ function Update-PowerShell {
   
 
   Write-Log -Message "Script is running as $($MyInvocation.MyCommand.Name)" -level info
+
+  function Get-PowerShellVersion {
+    param (
+      [string]$PowerShellPath
+    )
+
+    # StartInfo properties
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = $PowerShellPath
+    $startInfo.Arguments = '-Command $PSVersionTable.PSVersion'
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    $startInfo.CreateNoWindow = $true
+
+    # Create and start the process
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start() | Out-Null
+
+    # Read the output
+    $output = $process.StandardOutput.ReadToEnd()
+
+    # Wait for the process to exit
+    $process.WaitForExit()
+
+    return $output.Trim()
+  }
+
+
+
   $PSVersionsToCheck = @('Preview', 'Stable')
   $PSVersionsToCheck | ForEach-Object {
     # Check the currently installed version
     # Get the current PowerShell version
     if (Test-Path $PowershellStablePath) {
-      $CurrentVersionStable = & $PowershellStablePath -Command { $PSVersionTable.PSVersion }
+      $CurrentVersionStable = Get-PowerShellVersion -PowerShellPath $PowershellStablePath      
       Write-Log -Message "Current PowerShell Version: $CurrentVersionStable" -Level INFO
     }
     else {
@@ -27,7 +57,7 @@ function Update-PowerShell {
 
     # Check for PowerShell Preview version
     if (Test-Path $PowershellPreviewPath) {
-      $CurrentVersionPreview = & $PowershellPreviewPath -Command { $PSVersionTable.PSVersion }
+      $CurrentVersionPreview = Get-PowerShellVersion -PowerShellPath $PowershellPreviewPath
       Write-Log -Message "PowerShell Preview Version: $previewVersion" -Level INFO
     }
     else {
@@ -40,10 +70,8 @@ function Update-PowerShell {
     if ($_ -eq 'Preview') {
       $LatestVersionPreview = Get-LatestGitHubRelease -OwnerRepository 'PowerShell/PowerShell' -VersionOnly -PreRelease
       # compare the versions
-      if ($LatestVersionPreview -gt $CurrentVersionPreview) {
-        
-        $script:UpdatePreview = $true
-        return
+      if ($LatestVersionPreview -gt $CurrentVersionPreview) {        
+        $script:UpdatePreview = $true        
       }
 
     }
@@ -58,9 +86,21 @@ function Update-PowerShell {
     # Compare the versions
     if ($script:UpdatePreview -or $script:UpdateStable) {
       # If the latest version is newer than the current version, download the latest version
-      $DownloadPath = Get-LatestGitHubRelease -OwnerRepository 'PowerShell/PowerShell' $(if ($_ -eq 'Preview') {
-          -PreRelease
-        } ) -AssetName '*win-x64.msi' -DownloadPathDirectory $ENV:TEMP -Verbose -UseAria2  
+      $params = @{
+        OwnerRepository       = 'PowerShell/PowerShell'
+        AssetName             = '*win-x64.msi'
+        DownloadPathDirectory = $ENV:TEMP
+        Verbose               = $true
+        UseAria2              = $true
+      }
+
+      # Conditionally add the PreRelease switch if needed
+      if ($_ -eq 'Preview') {
+        $params['PreRelease'] = $true
+      }
+
+      # Call the function with the parameters
+      $DownloadPath = Get-LatestGitHubRelease @params
       # Install the latest version silently
       Start-Process -FilePath msiexec.exe -ArgumentList "/i $DownloadPath /quiet /norestart" -Wait
       # Remove the installer
@@ -69,6 +109,5 @@ function Update-PowerShell {
     Write-Log -Message "Powershell $_ is up to date" -level info
 
   } 
-
 
 }
