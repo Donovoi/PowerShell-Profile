7bf814c1c44964db5fb91bfec09e1b7348993f77
michael <michael@theablecoder.io>
2023-08-29 07:05:57 +1000

    choco fix

--- functions/Update-USBTools.ps1
+++ functions/Update-USBTools.ps1
@@ -2,7 +2,7 @@ function Update-USBTools {
   [cmdletbinding()]
   param(
   )
-  Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "chocolatey upgrade chocolatey.extension;chocolatey upgrade all"'
+  Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "chocolatey upgrade all"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-VcRedist -Verbose"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget install JanDeDobbeleer.OhMyPosh -s winget --force --accept-source-agreements --accept-package-agreements"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-VisualStudio -Verbose"'
@@ -12,7 +12,7 @@ function Update-USBTools {
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-PowerShell -Verbose"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Get-LatestSIV -Verbose"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget source reset --disable-interactivity --force"'
-  Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget source update --disable-interactivity --force"'
+  Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget source update --disable-interactivity"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "winget upgrade --all --include-unknown --wait -h --force --accept-source-agreements --accept-package-agreements"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "DISM /Online /Cleanup-Image /RestoreHealth; sfc /scannow"'
   Start-Process -FilePath pwsh.exe -ArgumentList '-noexit -command "Update-DotNetSDK -Verbose"'
