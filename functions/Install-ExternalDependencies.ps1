53d25454ba9b951cc4d1072832e60db045761f6f
Your Name <you@example.com>
2023-10-02 20:38:59 +1100

    add function

--- functions/Install-ExternalDependencies.ps1
+++ functions/Install-ExternalDependencies.ps1
@@ -373,6 +373,59 @@ function Update-SessionEnvironment {
     }
 }
 
+function Get-EnvironmentVariable {
+
+    [CmdletBinding()]
+    [OutputType([string])]
+    param(
+        [Parameter(Mandatory = $true)][string] $Name,
+        [Parameter(Mandatory = $true)][System.EnvironmentVariableTarget] $Scope,
+        [Parameter(Mandatory = $false)][switch] $PreserveVariables = $false,
+        [parameter(ValueFromRemainingArguments = $true)][Object[]] $ignoredArguments
+    )
+
+    [string] $MACHINE_ENVIRONMENT_REGISTRY_KEY_NAME = "SYSTEM\CurrentControlSet\Control\Session Manager\Environment\";
+    [Microsoft.Win32.RegistryKey] $win32RegistryKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey($MACHINE_ENVIRONMENT_REGISTRY_KEY_NAME)
+    if ($Scope -eq [System.EnvironmentVariableTarget]::User) {
+        [string] $USER_ENVIRONMENT_REGISTRY_KEY_NAME = "Environment";
+        [Microsoft.Win32.RegistryKey] $win32RegistryKey = [Microsoft.Win32.Registry]::CurrentUser.OpenSubKey($USER_ENVIRONMENT_REGISTRY_KEY_NAME)
+    }
+    elseif ($Scope -eq [System.EnvironmentVariableTarget]::Process) {
+        return [Environment]::GetEnvironmentVariable($Name, $Scope)
+    }
+
+    [Microsoft.Win32.RegistryValueOptions] $registryValueOptions = [Microsoft.Win32.RegistryValueOptions]::None
+
+    if ($PreserveVariables) {
+        Write-Verbose "Choosing not to expand environment names"
+        $registryValueOptions = [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames
+    }
+
+    [string] $environmentVariableValue = [string]::Empty
+
+    try {
+        #Write-Verbose "Getting environment variable $Name"
+        if ($null -ne $win32RegistryKey) {
+            # Some versions of Windows do not have HKCU:\Environment
+            $environmentVariableValue = $win32RegistryKey.GetValue($Name, [string]::Empty, $registryValueOptions)
+        }
+    }
+    catch {
+        Write-Debug "Unable to retrieve the $Name environment variable. Details: $_"
+    }
+    finally {
+        if ($null -ne $win32RegistryKey) {
+            $win32RegistryKey.Close()
+        }
+    }
+
+    if ($null -eq $environmentVariableValue -or $environmentVariableValue -eq '') {
+        $environmentVariableValue = [Environment]::GetEnvironmentVariable($Name, $Scope)
+    }
+
+    return $environmentVariableValue
+}
+
 function Get-EnvironmentVariableNames([System.EnvironmentVariableTarget] $Scope) {
 
     # HKCU:\Environment may not exist in all Windows OSes (such as Server Core).
