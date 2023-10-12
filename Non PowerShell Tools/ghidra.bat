3e73a96b871495d0461ae61f60ea10fbb4f0745f
Your Name <you@example.com>
2023-10-13 08:14:06 +1100

    fix

--- Non PowerShell Tools/ghidra.bat
+++ Non PowerShell Tools/ghidra.bat
@@ -1,16 +1,8 @@
-@echo off
-setlocal
-:: Find and strip the batch portion of the script, then pipe the rest to PowerShell which invokes pwsh to run the commands
-@findstr /v "^@findstr.*&" "%~f0" | powershell -noprofile -noexit -Command "pwsh -noprofile -noexit -Command { $input | Invoke-Expression }" & goto:eof
+@echo off && @findstr /v "^@echo.*&" "%~f0" | powershell -noprofile -noexit - & pause
 
-:: The label is the name of your USB drive.
+# The label is the name of your USB drive.
 $XWAYSUSB = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter
-$ghidraBaseDir = Join-Path -Path $XWAYSUSB -ChildPath "chocolatey apps\chocolatey\bin\lib\ghidra\tools" -Resolve
-$ghidraDir = Get-ChildItem -Path $ghidraBaseDir | Sort-Object -Property Name -Descending | Select-Object -First 1
+$ghidraBaseDir = Join-Path -Path $XWAYSUSB -ChildPath "\chocolatey apps\chocolatey\bin\lib\ghidra\tools" -Resolve
+$ghidraDir = Get-ChildItem -Path $ghidraBaseDir -Directory | Sort-Object -Property LastWriteTime -Descending | Select-Object -First 1
 $ghidraRunPath = Join-Path -Path $ghidraDir.FullName -ChildPath "ghidraRun.bat" -Resolve
-
-if (Test-Path $ghidraRunPath) {
-    Start-Process $ghidraRunPath
-} else {
-    Write-Host "ghidraRun.bat not found."
-}
+cmd.exe /c "$ghidraRunPath"
