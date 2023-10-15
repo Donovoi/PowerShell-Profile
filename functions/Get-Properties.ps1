a0270eab21fa3ca376c7a66fcc4e7e3ce39941d9
Your Name <you@example.com>
2023-10-15 17:41:10 +1100

    update get properties

--- functions/Get-Properties.ps1
+++ functions/Get-Properties.ps1
@@ -1,31 +1,53 @@
 function Get-Properties {
     param(
-        [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
         [PSObject]
         $Object
     )
 
-    process {
-        if ($Object -is [System.Collections.Specialized.OrderedDictionary]) {
-            foreach ($key in $Object.Keys) {
-                Write-Log -Message ("Name: " + $key)
-                Write-Log -Message ("Type: " + $Object[$key].GetType().FullName)
-                Write-Log -Message ("Value: " + $Object[$key])
+    # Initialize a HashSet to keep track of visited objects and prevent infinite recursion
+    $visited = New-Object 'System.Collections.Generic.HashSet[System.Object]'
 
-                if ($Object[$key] -is [psobject]) {
-                    Get-Properties -Object $Object[$key]
+    # Helper function for recursion
+    function Get-NestedProperties {
+        param(
+            [Parameter(Mandatory = $true)]
+            [PSObject]
+            $NestedObject,
+            [int]
+            $Depth = 0
+        )
+        
+        # Prefix for indentation
+        $prefix = "-" * $Depth
+        
+        if ($visited.Add($NestedObject)) {
+            if ($NestedObject -is [System.Collections.Specialized.OrderedDictionary]) {
+                foreach ($key in $NestedObject.Keys) {
+                    Write-Host "${prefix}Name: $key"
+                    Write-Host "${prefix}Type: $($NestedObject[$key].GetType().FullName)"
+                    Write-Host "${prefix}Value: $($NestedObject[$key])"
+                    Get-NestedProperties -NestedObject $NestedObject[$key] -Depth ($Depth + 1)
                 }
             }
-        } else {
-            $Object.PSObject.Properties | ForEach-Object {
-                Write-Log -Message ("Name: " + $_.Name)
-                Write-Log -Message ("Type: " + $_.TypeNameOfValue)
-                Write-Log -Message ("Value: " + $_.Value)
-
-                if ($_.Value -is [psobject]) {
-                    Get-Properties -Object $_.Value
+            elseif ($NestedObject -is [System.Collections.ICollection] -and $NestedObject -is [System.Array]) {
+                # Handle arrays
+                $NestedObject | ForEach-Object {
+                    Get-NestedProperties -NestedObject $_ -Depth ($Depth + 1)
+                }
+            }
+            else {
+                $NestedObject.PSObject.Properties | ForEach-Object {
+                    Write-Host "${prefix}Name: $($_.Name)"
+                    Write-Host "${prefix}Type: $($_.TypeNameOfValue)"
+                    Write-Host "${prefix}Value: $($_.Value)"
+                    if ($null -ne $_.Value) {
+                        Get-NestedProperties -NestedObject $_.Value -Depth ($Depth + 1)
+                    }
                 }
             }
         }
     }
+    Get-NestedProperties -NestedObject $Object
+    
 }
