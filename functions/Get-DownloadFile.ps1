6fdbcaf3ec6b0633ace972c964a3c27de97bdd10
Your Name <you@example.com>
2023-10-30 11:24:51 +1100

    simplify

--- functions/Get-DownloadFile.ps1
+++ functions/Get-DownloadFile.ps1
@@ -97,122 +97,55 @@ function Get-DownloadFile {
             Mandatory = $false,
             ParameterSetName = 'Auth'
         )]
-        [switch]$IsPrivateRepo,
-
-        [Parameter(
-            Mandatory = $false
-        )]
-        [ValidateRange(1, 20)]
-        [int]$MaxConcurrentDownloads = 5
+        [switch]$IsPrivateRepo
     )
-    begin {
-        $downloadScript = {
-            param (
-                [string]$URL,
-                [string]$OutFile,
-                [string]$OutFileDirectory,
-                [switch]$UseAria2,
-                [string]$SecretName,
-                [string]$aria2cExe,
-                [System.Collections.IDictionary]$Headers,
-                [switch]$IsPrivateRepo
-            )
+    process {
+        try {
+            if ($UseAria2) {
+                # Get functions from my github profile
+                $functions = @("Write-Log", "Invoke-AriaDownload", "Install-ExternalDependencies", "Get-LatestGitHubRelease")
+                $functions.ForEach{
+                    $function = $_                        
+                    Out-Host -InputObject "Getting $function from https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/$function.ps1"
+                    $Webfunction = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/$function.ps1"
+                    $Webfunction.Content | Invoke-Expression
+                }
 
-            try {
-                if ($UseAria2) {
-                    # Get functions from my github profile
-                    $functions = @("Write-Log", "Invoke-AriaDownload", "Install-ExternalDependencies", "Get-LatestGitHubRelease")
-                    $functions.ForEach{
-                        $function = $_                        
-                        Out-Host -InputObject "Getting $function from https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/$function.ps1"
-                        $Webfunction = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/$function.ps1"
-                        $Webfunction.Content | Invoke-Expression
+                if (-not(Test-Path -Path $aria2cExe)) {
+                    Get-LatestGitHubRelease -OwnerRepository "aria2/aria2" -AssetName "aria2_win-64bit-build1.zip" -DownloadPathDirectory "C:\aria2"
+                    Expand-Archive -Path "C:\aria2\aria2_win-64bit-build1.zip" -DestinationPath "C:\aria2"
+                    $aria2cExe = $(Resolve-Path -Path "C:\aria2\aria2c.exe").Path
+                }
+                Write-Host "Using aria2c for download."
+
+                # If it's a private repo, handle the secret
+                if ($IsPrivateRepo) {
+                    # Install any needed modules and import them
+                    if (-not (Get-Module -Name Microsoft.PowerShell.SecretManagement) -or (-not (Get-Module -Name Microsoft.PowerShell.SecretStore))) {
+                        Install-ExternalDependencies -PSModules 'Microsoft.PowerShell.SecretManagement', 'Microsoft.PowerShell.SecretStore' -NoNugetPackages -RemoveAllModules
                     }
+                    if ($null -ne $SecretName) {
+                        # Validate the secret exists and is valid
+                        if (-not (Get-SecretInfo -Name $SecretName)) {
+                            Write-Log -Message "The secret '$SecretName' does not exist or is not valid." -Level ERROR
+                            throw
+                        }      
 
-                    if (-not(Test-Path -Path $aria2cExe)) {
-                        Get-LatestGitHubRelease -OwnerRepository "aria2/aria2" -AssetName "aria2_win-64bit-build1.zip" -DownloadPathDirectory "C:\aria2"
-                        Expand-Archive -Path "C:\aria2\aria2_win-64bit-build1.zip" -DestinationPath "C:\aria2"
-                        $aria2cExe = $(Resolve-Path -Path "C:\aria2\aria2c.exe").Path
-                    }
-                    Write-Host "Using aria2c for download."
-
-                    # If it's a private repo, handle the secret
-                    if ($IsPrivateRepo) {
-                        # Install any needed modules and import them
-                        if (-not (Get-Module -Name Microsoft.PowerShell.SecretManagement) -or (-not (Get-Module -Name Microsoft.PowerShell.SecretStore))) {
-                            Install-ExternalDependencies -PSModules 'Microsoft.PowerShell.SecretManagement', 'Microsoft.PowerShell.SecretStore' -NoNugetPackages -RemoveAllModules
-                        }
-                        if ($null -ne $SecretName) {
-                            # Validate the secret exists and is valid
-                            if (-not (Get-SecretInfo -Name $SecretName)) {
-                                Write-Log -Message "The secret '$SecretName' does not exist or is not valid." -Level ERROR
-                                throw
-                            }      
-
-                            Invoke-AriaDownload -URL $URL -OutFile $OutFile -Aria2cExePath $aria2cExe -SecretName $SecretName -Headers:$Headers
-                        }
-                    }
-                    else {
-                        Invoke-AriaDownload -URL $URL -OutFile $OutFile -Aria2cExePath $aria2cExe -Headers:$Headers
+                        Invoke-AriaDownload -URL $URL -OutFile $OutFile -Aria2cExePath $aria2cExe -SecretName $SecretName -Headers:$Headers
                     }
                 }
                 else {
-                    Write-Host "Using Invoke-WebRequest for download."
-                    Invoke-WebRequest -Uri $URL -OutFile $OutFile -Headers $Headers
+                    Invoke-AriaDownload -URL $URL -OutFile $OutFile -Aria2cExePath $aria2cExe -Headers:$Headers
                 }
             }
-            catch {
-                Write-Host "An error occurred: $_" -ForegroundColor Red
-                throw
+            else {
+                Write-Host "Using Invoke-WebRequest for download."
+                Invoke-WebRequest -Uri $URL -OutFile $OutFile -Headers $Headers
             }
         }
-
-        $runspacePool = [RunspaceFactory]::CreateRunspacePool(1, $MaxConcurrentDownloads)
-        $runspacePool.Open()
-    }
-
-    process {
-        $jobs = @()
-        foreach ($URL in $URLs) {
-            $OutFile = Join-Path -Path $OutFileDirectory -ChildPath ($URL | Split-Path -Leaf)
-            $params = @{
-                URL           = $URL
-                OutFile       = $OutFile
-                UseAria2      = $UseAria2
-                aria2cExe     = $aria2cExe
-                SecretName    = $SecretName
-                Headers       = $Headers
-                IsPrivateRepo = $IsPrivateRepo
-            }
-            
-            $job = [powershell]::Create().AddScript($downloadScript).AddParameters($params)
-            $job.RunspacePool = $runspacePool
-            
-            $result = $job.BeginInvoke()
-            $jobs += @{
-                Job    = $job
-                Result = $result
-            }
+        catch {
+            Write-Host "An error occurred: $_" -ForegroundColor Red
+            throw
         }
-
-        $jobs.Result | ForEach-Object { $_.AsyncWaitHandle.WaitOne() }
-        
-        $jobs | ForEach-Object {
-            $jobOutput = $_.Job.EndInvoke($_.Result)
-            $jobError = $_.Job.Streams.Error
-            $_.Job.Dispose()
-            
-            if ($jobOutput) {
-                Write-Output "Job completed successfully: $($jobOutput)"
-            }
-            
-            if ($jobError) {
-                Write-Error "Error occurred: $($jobError)"
-            }
-        }
-    }
-
-    end {
-        $runspacePool.Close()
     }
 }
