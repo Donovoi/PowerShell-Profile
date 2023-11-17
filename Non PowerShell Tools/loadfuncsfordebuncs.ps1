21b3d9d06956fe12155403449c8b10b7ba78b945
Your Name <you@example.com>
2023-11-17 15:42:25 +1100

    Refactor script to exclude specific files from import

--- Non PowerShell Tools/loadfuncsfordebuncs.ps1
+++ Non PowerShell Tools/loadfuncsfordebuncs.ps1
@@ -1,6 +1,7 @@
 # get all files in the current directory except this file and import it as a script
 $ActualScriptName = Get-PSCallStack | Select-Object -First 1 -ExpandProperty ScriptName
 $ScriptParentPath = Split-Path -Path $(Resolve-Path -Path $($ActualScriptName.foreach{ $_ }) ) -Parent
-Get-ChildItem -Path $ScriptParentPath -Filter "*.ps1" -Exclude $(Get-PSCallStack).ScriptName | ForEach-Object {
+$scriptstonotimport = @("$($($ActualScriptName.foreach{$_}).split('\')[-1])", "Get-KapeAndTools.ps1", "*RustandFriends*", "*zimmerman*", "*memorycapture*" )
+Get-ChildItem -Path $ScriptParentPath -Exclude $scriptstonotimport | ForEach-Object {
     . $_.FullName
 }
