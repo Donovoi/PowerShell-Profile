e1108bd12c9d5622e375717171a3c9ae1e33e6b2
Your Name <you@example.com>
2023-11-30 22:11:38 +1100

    feat(download): enhance Invoke-AriaDownload with multiple sources and logging
    The Invoke-AriaDownload function has been updated to support downloading from a list of URLs provided in a file, as well as specifying a download directory. It now accepts optional parameters for setting the console log level for aria2c and handling multiple network interfaces. The commit introduces validation to ensure that either a single URL or a URL file is specified, but not both. Additionally, it includes logic to manage output directories and filenames dynamically based on input parameters.
    
    This enhancement improves the flexibility and usability of the download function, particularly for users who need to download multiple files or use specific network interfaces.

--- functions/Invoke-AriaDownload.ps1
+++ functions/Invoke-AriaDownload.ps1
@@ -1,52 +1,100 @@
 <#
- .SYNOPSIS
- This function downloads a file from a given URL using aria2c.
- 
- .DESCRIPTION
- The Invoke-AriaDownload function uses aria2c to download a file from a provided URL.
- If the output file already exists, it will be removed before the download starts.
- 
- .PARAMETER URL
- The URL of the file to download.
- 
- .PARAMETER OutFile
- The name of the output file.
- 
- .PARAMETER Aria2cExePath
- The path to the aria2c executable.
- 
- .PARAMETER SecretName
- The name of the secret in the secret store which contains the GitHub Personal Access Token.
- 
- .EXAMPLE
- Invoke-AriaDownload -URL "http://example.com/file.zip" -OutFile "C:\Downloads\file.zip" -Aria2cExePath "C:\path\to\aria2c.exe" -SecretName "GitHubPAT"
- 
- .NOTES
- Make sure aria2c is installed and accessible from your PATH.
- #>
+.SYNOPSIS
+This function downloads a file from a given URL using aria2c.
+
+.DESCRIPTION
+The Invoke-AriaDownload function uses aria2c to download a file from a provided URL.
+If the output file already exists, it will be removed before the download starts.
+
+.PARAMETER URL
+The URL of the file to download.
+
+.PARAMETER OutFile
+The name of the output file.
+
+.PARAMETER Aria2cExePath
+The path to the aria2c executable.
+
+.PARAMETER SecretName
+The name of the secret in the secret store which contains the GitHub Personal Access Token.
+
+.EXAMPLE
+$links = 'driversforlinuxurls.txt'
+Invoke-AriaDownload -AriaConsoleLogLevel notice -URLFile $links -DownloadDirectory 'H:\5820 drivers' -Aria2cExePath 'H:\chocolatey apps\chocolatey\bin\bin\aria2c.exe'
+
+.NOTES
+Make sure aria2c is installed and accessible from your PATH.
+#>
 function Invoke-AriaDownload {
     [CmdletBinding()]
     [OutputType([string])]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'URL')]
+        [ValidateScript({
+                if (-not $_ -and -not $PSBoundParameters['URLFile']) {
+                    throw 'Either URL or URLFile must be specified.'
+                }
+                elseif ($_ -and $PSBoundParameters['URLFile']) {
+                    throw 'Only one of URL or URLFile can be specified.'
+                }
+                return $true
+            })]
         [string]$URL,
-        
-        [Parameter(Mandatory = $true)]
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'URLFile')]
+        [ValidateScript({
+                if (-not $_ -and -not $PSBoundParameters['URL']) {
+                    throw 'Either URLFile or URL must be specified.'
+                }
+                elseif ($_ -and $PSBoundParameters['URL']) {
+                    throw 'Only one of URLFile or URL can be specified.'
+                }
+                return $true
+            })]
+        [string]$URLFile,
+
+        # Here we do the same thing as the two parameters above, but we do it in the ValidateScript block
+        [Parameter(Mandatory = $false, ParameterSetName = 'URL')]
+        [ValidateScript({
+                if (-not $_ -and -not $PSBoundParameters['DownloadDirectory']) {
+                    throw 'Either URL or DownloadDirectory must be specified.'
+                }
+                elseif ($_ -and $PSBoundParameters['DownloadDirectory']) {
+                    throw 'Only one of URL or DownloadDirectory can be specified.'
+                }
+                return $true
+            })]
         [string]$OutFile,
-        
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'URLFile')]
+        [ValidateScript({
+                if (-not $_ -and -not $PSBoundParameters['OutFile']) {
+                    throw 'Either URLFile or OutFile must be specified.'
+                }
+                elseif ($_ -and $PSBoundParameters['OutFile']) {
+                    throw 'Only one of URLFile or OutFile can be specified.'
+                }
+                return $true
+            })]
+        [string]$DownloadDirectory,
+
         [Parameter(Mandatory = $true)]
         [string]$Aria2cExePath,
-        
+
         [Parameter(Mandatory = $false)]
         [string]$SecretName,
-        
+
         [Parameter(Mandatory = $false)]
-        [System.Collections.IDictionary]$Headers
+        [System.Collections.IDictionary]$Headers,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('debug', 'info', 'notice', 'warn', 'error')]
+        [string]$AriaConsoleLogLevel = 'info'
     )
     begin {
         # Print the name of the running script
-        Write-Host 'Downloading Faster? with Aria2' -ForegroundColor Green
-        
+        Write-Output 'Downloading With Aria2c'
+
         # Ensure aria2c is in the PATH
         if (-not (Test-Path -Path $Aria2cExePath)) {
             throw "aria2c was not found. Make sure you have the right path for $Aria2cExePath"
@@ -59,10 +107,10 @@ function Invoke-AriaDownload {
             if (Test-Path $OutFile) {
                 Remove-Item -Path $OutFile -Force -ErrorAction Stop
             }
-            
+
             # Construct the authorization header if a valid secret name is provided and the url is from github
-            $authHeader = ""
-            if ($URL -like '*github.com*') {               
+            $authHeader = ''
+            if ($URL -like '*github.com*') {
                 if (-not [string]::IsNullOrEmpty($SecretName)) {
                     # Install any needed modules and import them
                     if (-not (Get-Module -Name Microsoft.PowerShell.SecretManagement) -or (-not (Get-Module -Name Microsoft.PowerShell.SecretStore))) {
@@ -74,7 +122,7 @@ function Invoke-AriaDownload {
                     }
                 }
             }
-            
+
             # Create an array to hold header arguments for aria2c
             $headerArgs = @()
             if ($Headers) {
@@ -82,25 +130,65 @@ function Invoke-AriaDownload {
                     $headerArgs += "--header=`"$key`: $($Headers[$key])`""
                 }
             }
-            
+            # Get all interfaces that can download the file
+            $interfaces = @()
+            Get-NetAdapter | Where-Object { $_.Status -eq 'Up' } | ForEach-Object {
+                $adapter = $_
+                Get-NetIPAddress -InterfaceIndex $adapter.ifIndex | Where-Object {
+                    $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -ne 'WellKnown' -and $_.SuffixOrigin -ne 'WellKnown'
+                } | ForEach-Object {
+                    $interfaces += $_.IPAddress
+                }
+            }
+
+            # Join the IP addresses into a single string separated by commas
+            $interfaceString = $interfaces -join ','
+
+            # while downloading we need to be in the same directory as the output file (because spaces in the path)
+            if ($OutFile) {
+                $Outdir = $(Split-Path -Path $OutFile -Parent)
+                Push-Location -Path $Outdir
+            }
+            else {
+                Push-Location -Path $DownloadDirectory
+            }
+
+            if ($URL) {
+                $urlfileargument = ''
+                $outfileargument = "--out=`"$(Split-Path -Leaf ${OutFile})`""
+            }
+            else {
+                $URL = ''
+                $outfileargument = ''
+                $urlfileargument = "--input-file=$URLFile"
+            }
             # Start the download process using aria2c
-            Start-Process -FilePath $Aria2cExePath -ArgumentList @(
-                '--file-allocation=none',
+            $asciiEncoding = [System.Text.Encoding]::ASCII
+            $ariaarguments = @(
+                "--console-log-level=$AriaConsoleLogLevel"
                 '--continue=false',
+                $urlfileargument,
                 '--max-connection-per-server=16',
-                "--log=$($(Split-Path -Parent `"$OutFile`") + '\aria2c.log')",
+                '--max-concurrent-downloads=16',
+                '--log=aria2c.log',
                 '--disable-ipv6',
                 '--split=16',
                 '--min-split-size=1M',
+                '--file-allocation=trunc',
+                '--enable-mmap=true',
                 '--max-tries=0',
+                "--multiple-interface=$interfaceString",
                 '--allow-overwrite=true',
-                "--dir=$(Split-Path -Parent `"$OutFile`")",
-                "--out=$(Split-Path -Leaf `"$OutFile`")",
+                $outfileargument,
                 $headerArgs.GetEnumerator(), # Include custom headers if provided
                 $authHeader, # Include the authorization header if it was constructed
-                $URL
-            ) -NoNewWindow -Wait
+                $URL  # Use the URL if provided, otherwise use the URLFile
+            ).ForEach({ $asciiEncoding.GetString($asciiEncoding.GetBytes($_)) })
 
+            Start-Process -FilePath $Aria2cExePath -ArgumentList $ariaarguments -NoNewWindow -Wait
+
+            # Return the output file path
+            Pop-Location
             return $OutFile
         }
         catch {
@@ -108,3 +196,5 @@ function Invoke-AriaDownload {
         }
     }
 }
+
+
