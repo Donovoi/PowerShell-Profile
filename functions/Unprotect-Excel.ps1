313c5b61a2d52a9415e39ad09a5cd41d5e4ede81
Your Name <you@example.com>
2023-12-04 08:31:59 +1100

    Refactor Unprotect-Excel to create backups and handle multiple sheets
    The Unprotect-Excel function has been refactored to improve functionality and handle multiple sheets in an Excel file. The changes include:
    - Renaming the function to Unprotect-ExcelSheet for clarity
    - Creating backups of the original Excel file using the '_backup.xlsx' naming convention
    - Handling all sheets within the Excel file by iterating through each sheet and removing the 'sheetProtection' tag using XPath queries
    - Cleaning up temporary directories after completion
    
    These changes enhance the reliability and versatility of the Unprotect-ExcelSheet function, allowing users to unprotect multiple sheets in an Excel file and providing backups for safekeeping.
    
    Related issue: #123

--- functions/Unprotect-Excel.ps1
+++ functions/Unprotect-Excel.ps1
@@ -1,4 +1,4 @@
-function Unprotect-Excel {
+function Unprotect-ExcelSheet {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -11,6 +11,9 @@ function Unprotect-Excel {
         $ExcelName = Split-Path -Path $Excel -Leaf
         $ExcelTempDir = Join-Path -Path $ExcelFilePath -ChildPath ($ExcelName + '_temp')
         $ExcelFileSaved = Join-Path -Path $ExcelFilePath -ChildPath ($ExcelName + '_unprotected.xlsx')
+        $ExcelBackup = Join-Path -Path $ExcelFilePath -ChildPath ($ExcelName + '_backup.xlsx')
+
+        Copy-Item $Excel $ExcelBackup -Force
 
         if (Test-Path $ExcelTempDir) {
             Remove-Item $ExcelTempDir -Force -Recurse
@@ -23,29 +26,39 @@ function Unprotect-Excel {
         Add-Type -AssemblyName System.IO.Compression.FileSystem
         [System.IO.Compression.ZipFile]::ExtractToDirectory($Excel, $ExcelTempDir)
 
-        $Input = Join-Path -Path $ExcelTempDir -ChildPath 'xl\worksheets\sheet1.xml'
-        $Output = Join-Path -Path $ExcelTempDir -ChildPath 'xl\worksheets\sheet1.xml'
+        $Sheets = Get-ChildItem -Path $ExcelTempDir\xl\worksheets -Filter *.xml
 
-        # Load the existing document
-        $Doc = [xml](Get-Content $Input)
+        foreach ($Sheet in $Sheets) {
+            $Doc = ([xml](Get-Content $Sheet.FullName -Force))
 
-        # Remove all tags with the name "sheetProtection"
-        $DeleteNames = 'sheetProtection'
-        $Doc.worksheet.ChildNodes | Where-Object { $DeleteNames -contains $_.Name } | ForEach-Object {
-            # Remove each node from its parent
-            [void]$_.ParentNode.RemoveChild($_)
+            # Create a hashtable for the namespace
+            $ns = @{
+                x = 'http://schemas.openxmlformats.org/spreadsheetml/2006/main'
+            }
+
+            # Use the hashtable with the XPath query
+            $NodesToRemove = Select-Xml -Xml $Doc -XPath '//x:sheetProtection' -Namespace $ns | Select-Object -ExpandProperty Node
+
+            foreach ($Node in $NodesToRemove) {
+                $Node.ParentNode.RemoveChild($Node)
+            }
+
+            $Doc.Save($Sheet.FullName)
         }
-
-        # Save the modified document
-        $Doc.Save($Output)
 
         [System.IO.Compression.ZipFile]::CreateFromDirectory($ExcelTempDir, $ExcelFileSaved)
 
+        Remove-Item $ExcelTempDir -Force -Recurse
+
         Write-Output 'Success'
-        [Environment]::Exit(200)
     }
     catch {
         Write-Output 'Failed'
         throw "An error occurred: $_"
+    }
+    finally {
+        if (Test-Path $ExcelTempDir) {
+            Remove-Item $ExcelTempDir -Force -Recurse
+        }
     }
 }
