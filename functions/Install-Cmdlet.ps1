e5612fb1a8b4b67e56f958effff3ff2fd32060b4
Your Name <you@example.com>
2023-12-05 21:15:23 +1100

    feat(powershell): add Install-Cmdlet function for installing cmdlets from URLs
    This commit introduces a new PowerShell function, Install-Cmdlet, which allows users to install cmdlets from specified URLs. The function validates the URL using regex, downloads the script content using Invoke-RestMethod, and creates a new PowerShell module from the script block that encapsulates the downloaded content. It also handles exporting of module members and provides an option to specify a particular cmdlet to install. This enhancement simplifies the process of extending PowerShell functionality with custom cmdlets sourced from external URLs.

---
+++ functions/Install-Cmdlet.ps1
@@ -0,0 +1,59 @@
+# help comment block below very detailed
+<#
+.SYNOPSIS
+    Install-Cmdlet is a function that installs a cmdlet from a given url.
+
+.DESCRIPTION
+    The Install-Cmdlet function takes a url as input and installs a cmdlet from the content obtained from the url.
+    It creates a new PowerShell module from the content obtained from the URI.
+    'Invoke-RestMethod' is used to download the script content.
+    The script content is encapsulated in a script block and a new module is created from it.
+    'Export-ModuleMember' exports all functions and aliases from the module.
+    The function can be used to install cmdlets from a given url.
+
+.PARAMETER Url
+    The url from which the cmdlet should be installed.
+
+.PARAMETER CmdletToInstall
+    The cmdlet that should be installed from the given url.
+    If no cmdlet is specified, all cmdlets from the given url will be installed.
+
+.EXAMPLE
+    Install-Cmdlet -Url 'https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/Write-Logg.ps1' -CmdletToInstall 'Write-Logg'
+    This example installs the Write-Logg cmdlet from the given url.
+
+#>
+
+function Install-Cmdlet {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Url,
+        [Parameter(Mandatory = $false)]
+        [string]
+        $CmdletToInstall = '*'
+    )
+    
+    begin {
+        # make sure we are given a valid url
+        $searchpattern = "((ht|f)tp(s?)\:\/\/?)[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&%\$#_]*)?"
+        $regexoptions = [System.Text.RegularExpressions.RegexOptions]('IgnoreCase, IgnorePatternWhitespace, Compiled')
+        $compiledregex = [regex]::new($searchpattern, $regexoptions)
+        if (-not($url -match $compiledregex)) {
+            throw [System.ArgumentException]::new('The given url is not valid')
+        }
+    }
+    process {
+        try {
+            $method = Invoke-RestMethod -Uri $Url
+            $CmdletScriptBlock = [scriptblock]::Create($method.ToString() + "`nExport-ModuleMember -Function * -Alias *")
+            New-Module -Name 'WriteLoggModule' -ScriptBlock $CmdletScriptBlock | Import-Module -Cmdlet $CmdletToInstall
+        }
+        catch {
+            throw $_
+        }
+    }
+    end {
+    }
+}
