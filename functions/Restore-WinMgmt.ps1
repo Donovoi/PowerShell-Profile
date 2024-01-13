2c49e95204cfef910f3c1e078236ead0286198dc
Your Name <you@example.com>
2024-01-14 02:19:35 +1100

    Fix WinMgmt start-up and repository issues

--- functions/Restore-WinMgmt.ps1
+++ functions/Restore-WinMgmt.ps1
@@ -3,12 +3,18 @@ function Restore-WinMgmt {
   param(
 
   )
-  if (-not (Get-Disk)) {
-    Set-Content config winmgmt start= disabled
+  if (-not (Get-Disk -ErrorAction SilentlyContinue)) {
+    # all commands below must be run in cmd as admin
+    $commands = @'
+    sc config winmgmt start= disabled
     net stop winmgmt
     Winmgmt /salvagerepository %windir%\System32\wbem
     Winmgmt /resetrepository %windir%\
-    Set-Content config Winmgmt start= auto
+    sc config winmgmt start= auto
+'@
+    $scriptBlock = [scriptblock]::Create($commands)
+    Start-Process 'cmd' -ArgumentList '/c', $scriptBlock -Verb RunAs -Wait
   }
+  Write-Output 'WinMgmt has been restored.'
 }
 
