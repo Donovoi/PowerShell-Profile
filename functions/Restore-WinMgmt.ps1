9555fb76327c2e055bfc534aa1860e04ca5d8cc7
Your Name <you@example.com>
2024-01-14 02:41:32 +1100

    Fix WinMgmt restoration process

--- functions/Restore-WinMgmt.ps1
+++ functions/Restore-WinMgmt.ps1
@@ -3,19 +3,24 @@ function Restore-WinMgmt {
   param(
     [switch]$Force
   )
+  #requires -RunAsAdministrator
   if ((-not (Get-Disk -ErrorAction SilentlyContinue)) -or ($Force -eq $true)) {
     # all commands below must be run in cmd as admin
-    $commands = @'
-    sc config winmgmt start= disabled
-    net stop winmgmt
-    Winmgmt /salvagerepository %windir%\System32\wbem
-    Winmgmt /resetrepository %windir%\
-    sc config winmgmt start= auto
-'@
-    $scriptBlock = [scriptblock]::Create($commands)
-    Start-Process 'cmd' -ArgumentList '/c', $scriptBlock -Verb RunAs -Wait
+    $commands = @(
+      'sc config winmgmt start= disabled',
+      'net stop winmgmt',
+      'Winmgmt /salvagerepository %windir%\System32\wbem',
+      'Winmgmt /resetrepository %windir%',
+      'sc config winmgmt start= auto'
+    )
+
+    # Execute each command using cmd.exe /c
+    foreach ($command in $commands) {
+      Start-Process -FilePath 'cmd.exe' -ArgumentList "/c $command" -NoNewWindow -Wait
+    }
+
     Write-Output 'WinMgmt has been restored.'
   }
-  Write-Warning 'WinMgmt is running correctly And -Force has not been used Nothing to do.'
+  Write-Warning 'WinMgmt is running correctly And -Force has not been used. Nothing to do.'
 }
 
