c7881eaf7b579f1f668e6d75f67340972f6bb8a6
Your Name <you@example.com>
2024-01-14 02:26:15 +1100

    Fix WinMgmt restoration logic and add Force option

--- functions/Restore-WinMgmt.ps1
+++ functions/Restore-WinMgmt.ps1
@@ -1,9 +1,9 @@
 function Restore-WinMgmt {
   [CmdletBinding()]
   param(
-
+    [switch]$Force
   )
-  if (-not (Get-Disk -ErrorAction SilentlyContinue)) {
+  if ((-not (Get-Disk -ErrorAction SilentlyContinue)) -or ($Force -eq $true)) {
     # all commands below must be run in cmd as admin
     $commands = @'
     sc config winmgmt start= disabled
@@ -14,7 +14,8 @@ function Restore-WinMgmt {
 '@
     $scriptBlock = [scriptblock]::Create($commands)
     Start-Process 'cmd' -ArgumentList '/c', $scriptBlock -Verb RunAs -Wait
+    Write-Output 'WinMgmt has been restored.'
   }
-  Write-Output 'WinMgmt has been restored.'
+  Write-Warning 'WinMgmt is running correctly And -Force has not been used Nothing to do.'
 }
 
