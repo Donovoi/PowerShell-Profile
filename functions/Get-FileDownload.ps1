d5eb8706e392f5152b5c4756377979a1d52ce270
Your Name <you@example.com>
2024-02-28 12:51:58 +1100

    Refactor Get-FileDownload function to handle private repositories and install missing cmdlets

--- functions/Get-FileDownload.ps1
+++ functions/Get-FileDownload.ps1
@@ -58,13 +58,13 @@ function Get-FileDownload {
 
         [Parameter(
             Mandatory = $false
-            
+
         )]
         [switch]$UseAria2,
 
         [Parameter(
             Mandatory = $false
-           
+
         )]
         [ValidateScript({
                 if (-not (Test-Path $_ -PathType Leaf)) {
@@ -76,7 +76,7 @@ function Get-FileDownload {
 
         [Parameter(
             Mandatory = $false
-            
+
         )]
         [ValidateNotNullOrEmpty()]
         [string]$Token,
@@ -89,12 +89,25 @@ function Get-FileDownload {
 
         [Parameter(
             Mandatory = $false
-            
+
         )]
         [switch]$IsPrivateRepo
     )
     process {
         try {
+            $neededcmdlets = @('Install-Dependencies', 'Get-FileDownload', 'Get-LatestGitHubRelease', 'Invoke-AriaDownload', 'Get-LongName', 'Write-Logg', 'Get-Properties')
+            $neededcmdlets | ForEach-Object {
+                if (-not (Get-Command -Name $_ -ErrorAction SilentlyContinue)) {
+                    if (-not (Get-Command -Name 'Install-Cmdlet' -ErrorAction SilentlyContinue)) {
+                        $method = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/Install-Cmdlet.ps1'
+                        $finalstring = [scriptblock]::Create($method.ToString() + "`nExport-ModuleMember -Function * -Alias *")
+                        New-Module -Name 'InstallCmdlet' -ScriptBlock $finalstring | Import-Module
+                    }
+                    Write-Verbose -Message "Importing cmdlet: $_"
+                    $Cmdletstoinvoke = Install-Cmdlet -donovoicmdlets $_
+                    $Cmdletstoinvoke | Import-Module -Force
+                }
+            }
             if ($UseAria2) {
                 if (-not(Test-Path -Path $aria2cExe -ErrorAction SilentlyContinue)) {
                     $aria2directory = Get-LatestGitHubRelease -OwnerRepository 'aria2/aria2' -AssetName '-win-64bit-' -ExtractZip
@@ -125,13 +138,32 @@ function Get-FileDownload {
                 else {
                     # If the url does not have the filename in it, we get the filename from the headers
                     # Make a HEAD request to get headers
-                    $response = Invoke-WebRequest -Uri $download -Headers $Headers -Method Head
+                    if (-not $IsPrivateRepo) {
+                        $response = Invoke-WebRequest -Uri $download -Method Head
+                    }
+                    else {
+                        $headers = @{
+                            'Authorization' = "token $token"
+                            'User-Agent'    = 'PowerShell'
+                            # 'Accept'        = 'application/octet-stream'
+                            'Accept'        = 'application/vnd.github.v3+json'
+                        }
+
+                        $jsonresponse = Invoke-WebRequest -Uri $download -Method Get -Headers $headers
+                    }
+
                     $potentialFileNames = @()
 
                     # Check Content-Disposition first
                     $filenamematch = $false
-                    if ($response.Headers['Content-Disposition']) {
-                        $contentDisposition = $response.Headers['Content-Disposition']
+                    # We will do the easiest first, check if there is a filename in the header returned from github
+                    $parsedresponse = $jsonresponse.Content | ConvertFrom-Json
+                    if ($parsedresponse.Name) {
+                        $fileName = $parsedresponse.Name
+                        $potentialFileNames += $fileName                        
+                    }
+                    else {
+                        $contentDisposition = $jsonresponse.Headers['Content-Disposition']
                         $filenamematch = $([regex]::Match($contentDisposition, '([a-zA-Z0-9-.]{3,}\.[a-zA-Z0-9-.]{3,6})'))
                         if ($filenamematch.Success) {
                             $fileName = $filenamematch.Value
@@ -143,17 +175,16 @@ function Get-FileDownload {
                             }
                             $potentialFileNames += $fileName
                         }
-                    }
-                    if (-not ($filenamematch)) {
-                        # Regex search across all headers for additional filenames
-                        foreach ($header in $response.Headers.GetEnumerator()) {
-                            if ($header.Value -match '([a-zA-Z0-9-.]{3,}\.[a-zA-Z0-9-.]{3,6})') {
-                                $potentialFileNames += $matches[1]
+                        if (-not ($filenamematch)) {
+                            # Regex search across all headers for additional filenames
+                            foreach ($header in $response.Headers.GetEnumerator()) {
+                                if ($header.Value -match '([a-zA-Z0-9-.]{3,}\.[a-zA-Z0-9-.]{3,6})') {
+                                    $potentialFileNames += $matches[1]
+                                }
                             }
                         }
                     }
-
-
+                
                     # Determine the final filename
                     $finalFileName = $null
                     if ($potentialFileNames.Count -gt 0) {
@@ -177,11 +208,11 @@ function Get-FileDownload {
                     # If it's a private repo, handle the secret
                     if ($IsPrivateRepo) {
                         if ($null -ne $Token) {
-                            Invoke-AriaDownload -URL $download -OutFile $OutFile -Aria2cExePath $aria2cExe -Token:$Token -Headers:$Headers
+                            $DownloadedFile = Invoke-AriaDownload -URL $download -OutFile $OutFile -Aria2cExePath $aria2cExe -Token:$Token
                         }
                     }
                     else {
-                        $DownloadedFile = Invoke-AriaDownload -URL $download -OutFile $OutFile -Aria2cExePath $aria2cExe -Headers:$Headers
+                        $DownloadedFile = Invoke-AriaDownload -URL $download -OutFile $OutFile -Aria2cExePath $aria2cExe
                     }
                 }
                 else {
