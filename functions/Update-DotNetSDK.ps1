527741c0f493076c3d17df1315f793e828ae6642
Your Name <you@example.com>
2024-03-11 15:37:37 +1100

    Add missing cmdlets and update .NET SDK versions

--- functions/Update-DotNetSDK.ps1
+++ functions/Update-DotNetSDK.ps1
@@ -5,6 +5,19 @@ function Update-DotNetSDK {
         [string]
         $SearchPattern = 'Microsoft.DotNet.SDK'
     )
+    $neededcmdlets = @('Write-Logg')
+    $neededcmdlets | ForEach-Object {
+        if (-not (Get-Command -Name $_ -ErrorAction SilentlyContinue)) {
+            if (-not (Get-Command -Name 'Install-Cmdlet' -ErrorAction SilentlyContinue)) {
+                $method = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/Install-Cmdlet.ps1'
+                $finalstring = [scriptblock]::Create($method.ToString() + "`nExport-ModuleMember -Function * -Alias *")
+                New-Module -Name 'InstallCmdlet' -ScriptBlock $finalstring | Import-Module
+            }
+            Write-Verbose -Message "Importing cmdlet: $_"
+            $Cmdletstoinvoke = Install-Cmdlet -donovoicmdlets $_
+            $Cmdletstoinvoke | Import-Module -Force
+        }
+    }
 
     # Search for available .NET SDKs using winget
     $availableSDKs = winget search $SearchPattern | Where-Object { $_ -match 'Microsoft\.DotNet\.SDK\.' }
@@ -14,7 +27,7 @@ function Update-DotNetSDK {
 
     foreach ($DotNetVersion in $DotNetVersions) {
         Write-Logg -Message "Installing .NET SDK version $DotNetVersion"
-        winget install $("Microsoft.DotNet.SDK." + $DotNetVersion) --force --accept-source-agreements --accept-package-agreements
+        winget install $('Microsoft.DotNet.SDK.' + $DotNetVersion) --force --accept-source-agreements --accept-package-agreements
         Write-Logg -Message "Finished installing .NET SDK version $DotNetVersion"
     }
 }
