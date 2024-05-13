46024debe85eda7aa61bdd0572ead8d37e658dac
Your Name <you@example.com>
2024-05-13 11:16:42 +1000

    Refactor Update-VcRedist.ps1 to import required cmdlets and improve code readability

--- functions/Update-VcRedist.ps1
+++ functions/Update-VcRedist.ps1
@@ -8,15 +8,23 @@ function Update-VcRedist {
   param(
 
   )
-  Write-Logg -Message "Script is running as $($MyInvocation.MyCommand.Name)" -level info
-  Invoke-RestMethod 'https://api.github.com/repos/abbodi1406/vcredist/releases/latest' | ForEach-Object assets | Where-Object name -Like "*VisualCppRedist_AIO_x86_x64*.exe" | ForEach-Object {
-    Invoke-WebRequest $_.browser_download_url -OutFile $_.name
-    # Run the installer
-    Start-Process -FilePath $_.name -ArgumentList "/y" -Wait -NoNewWindow
+    
+  # Import the required cmdlets
+  $neededcmdlets = @('Install-Dependencies', 'Get-FileDownload', 'Invoke-AriaDownload', 'Get-LongName', 'Write-Logg', 'Get-Properties', 'Get-LatestGitHubRelease')
+  $neededcmdlets | ForEach-Object {
+    if (-not (Get-Command -Name $_ -ErrorAction SilentlyContinue)) {
+      if (-not (Get-Command -Name 'Install-Cmdlet' -ErrorAction SilentlyContinue)) {
+        $method = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/Install-Cmdlet.ps1'
+        $finalstring = [scriptblock]::Create($method.ToString() + "`nExport-ModuleMember -Function * -Alias *")
+        New-Module -Name 'InstallCmdlet' -ScriptBlock $finalstring | Import-Module
+      }
+      Write-Verbose -Message "Importing cmdlet: $_"
+      $Cmdletstoinvoke = Install-Cmdlet -donovoicmdlets $_
+      $Cmdletstoinvoke | Import-Module -Force
+    }
   }
-
-
-
-
+  Get-LatestGitHubRelease -OwnerRepository 'abbodi1406/vcredist' -AssetName 'VisualCppRedist_AIO_x86_x64' -DownloadPathDirectory $ENV:TEMP -UseAria2 -NoRPCMode
+  $installerPath = $(Get-ChildItem -Path "$ENV:TEMP\VisualCppRedist_AIO_x86_x64*" | Sort-Object -Property CreationTime -Descending | Select-Object -First 1).FullName
+  # Run the installer
+  Start-Process -FilePath $installerPath -ArgumentList '/y' -Wait
 }
-
