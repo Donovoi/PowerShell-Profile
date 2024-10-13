c4f0c904672da0b6a38cd0b9a233b2dc02e3a8cd
michael Moran <michael_john55@hotmail.com>
2024-10-14 07:40:11 +1100

    refactor: Update Get-FileDownload function to conditionally return downloaded file based on content

--- functions/Get-FileDownload.ps1
+++ functions/Get-FileDownload.ps1
@@ -265,6 +265,9 @@ function Get-FileDownload {
             Write-Error -Message "An error occurred: $_"
             throw
         }
-        return $Script:DownloadedFile
+        # only return the downloaded file if it is not empty and not containing the term *aria2*
+        if ($Script:DownloadedFile -and $Script:DownloadedFile -notlike '*aria2*') {
+            return $Script:DownloadedFile
+        }
     }
 }
