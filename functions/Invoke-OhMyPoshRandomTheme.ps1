9745c65e46b178b500804a6d98a86177d4e8d1f9
Your Name <you@example.com>
2024-10-21 10:28:32 +1100

    refactor: Suppress output of oh-my-posh initialization and update theme retrieval in Invoke-OhMyPoshRandomTheme function

--- functions/Invoke-OhMyPoshRandomTheme.ps1
+++ functions/Invoke-OhMyPoshRandomTheme.ps1
@@ -1,11 +1,11 @@
 function Invoke-OhMyPoshRandomTheme {
-    oh-my-posh init pwsh | Invoke-Expression
+    oh-my-posh init pwsh | Invoke-Expression | Out-Null
     # Get a list of all available Oh My Posh themes
-    $themes = Get-poshThemes | Out-Null #"$ENV:LOCALAPPDATA\Programs\oh-my-posh\themes" | Get-ChildItem -Filter '*.omp.json'
+    $themes = Get-poshThemes -list | Out-Null #"$ENV:LOCALAPPDATA\Programs\oh-my-posh\themes" | Get-ChildItem -Filter '*.omp.json'
 
     # Select a random theme
     $theme = Get-Random -InputObject $themes
 
     # Initialize Oh My Posh with the random theme
-    & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$theme.FullName" --print) -join "`n"))
+    & ([ScriptBlock]::Create((oh-my-posh init pwsh --config "$theme" --print) -join "`n"))
 }
