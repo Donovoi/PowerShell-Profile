8f91d7647c86ca8dbf7e50833f9152c7e83499ba
Your Name <you@example.com>
2024-10-21 10:54:08 +1100

    refactor: Enhance Invoke-OhMyPoshRandomTheme function to use an array for theme selection

--- functions/Invoke-OhMyPoshRandomTheme.ps1
+++ functions/Invoke-OhMyPoshRandomTheme.ps1
@@ -1,10 +1,18 @@
 function Invoke-OhMyPoshRandomTheme {
+    [CmdletBinding()]
+    param()
     oh-my-posh init pwsh | Invoke-Expression | Out-Null
     # Get a list of all available Oh My Posh themes
-    $themes = Get-poshThemes -list | Out-Null #"$ENV:LOCALAPPDATA\Programs\oh-my-posh\themes" | Get-ChildItem -Filter '*.omp.json'
+    $themes = Get-PoshThemes -Path $env:POSH_THEMES_PATH -List
+
+    # Create an empty array to store the themes
+    $themearray = @()
+
+    # create an array from the list of themes
+    $themearray += $themes | ForEach-Object { $_ }
 
     # Select a random theme
-    $theme = Get-Random -InputObject $themes
+    $theme = Get-Random -InputObject $themearray
 
     # Initialize Oh My Posh with the random theme
     oh-my-posh init pwsh --config $theme | Invoke-Expression
