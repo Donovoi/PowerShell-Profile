9faa9e7ed76ccb2903cc725beea30e164bd2ebec
Your Name <you@example.com>
2024-10-21 11:58:14 +1100

    refactor: Remove output suppression during theme retrieval in Invoke-OhMyPoshRandomTheme function

--- functions/Invoke-OhMyPoshRandomTheme.ps1
+++ functions/Invoke-OhMyPoshRandomTheme.ps1
@@ -5,20 +5,20 @@ function Invoke-OhMyPoshRandomTheme {
     # Specify a default theme configuration to prevent the banner
     $null = oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\default.omp.json" | Invoke-Expression | Out-Null
 
-    # Get the list of themes and suppress output
-    $themes = Get-PoshThemes -Path $env:POSH_THEMES_PATH -List -ErrorAction SilentlyContinue | Out-String | Out-Null
+    # Get the list of themes
+    $themes = Get-PoshThemes -Path $env:POSH_THEMES_PATH -List -ErrorAction SilentlyContinue | Out-String
 
-    # Process the themes list and suppress output
+    # Process the themes list, suppressing output from the loop
     $themesarray = @()
     $themes -split [System.Environment]::NewLine | ForEach-Object {
         if ($_ -like '*.omp.json') {
-            $themesarray += $_
+            [void]$themesarray.Add($_)
         }
-    } | Out-Null
+    }
 
     # Select a random theme
     $theme = Get-Random -InputObject $themesarray
 
-    # Initialize Oh My Posh with the random theme and suppress output
-    $null = oh-my-posh init pwsh --config $theme | Invoke-Expression | Out-Null
+    # Initialize Oh My Posh with the random theme
+    oh-my-posh init pwsh --config $theme | Invoke-Expression | Out-Null
 }
