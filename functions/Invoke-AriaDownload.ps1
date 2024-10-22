3f41fbb55a71906c29530e342499e27ef6150068
trex <trex@git.com>
2024-10-22 12:10:50 +1100

    refactor: Update UserAgent string in Invoke-AriaDownload function for compatibility

--- functions/Invoke-AriaDownload.ps1
+++ functions/Invoke-AriaDownload.ps1
@@ -100,7 +100,7 @@ function Invoke-AriaDownload {
         # useragent string
         [Parameter(Mandatory = $false)]
         [string]
-        $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+        $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
     )
     begin {
         $neededcmdlets = @('Test-InPath', 'Invoke-AriaRPCDownload')
