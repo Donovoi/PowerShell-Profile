3ad929e2bcd2cbc9f219706cc84bc326a466d2b4
Your Name <you@example.com>
2025-04-17 13:35:17 +1000

    fix: Remove unnecessary PreferLocal parameter from Install-Cmdlet invocation in Get-FileDownload function

--- functions/Get-FileDownload.ps1
+++ functions/Get-FileDownload.ps1
@@ -130,7 +130,7 @@ function Get-FileDownload {
                 New-Module -Name 'InstallCmdlet' -ScriptBlock $finalstring | Import-Module
             }
             Write-Verbose "Importing cmdlet: $cmd"
-            $scriptBlock = Install-Cmdlet -RepositoryCmdlets $cmd -PreferLocal -Force
+            $scriptBlock = Install-Cmdlet -RepositoryCmdlets $cmd -Force
 
             # Check if the returned value is a ScriptBlock and import it properly
             if ($scriptBlock -is [scriptblock]) {
