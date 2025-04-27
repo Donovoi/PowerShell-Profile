e4f7f998ad9a15523962878d78d30e8c05f3f3f2
Your Name <you@example.com>
2025-04-28 07:13:55 +1000

    fix: Update Chocolatey installation path logic in Update-Tools function

--- functions/Update-Tools.ps1
+++ functions/Update-Tools.ps1
@@ -41,6 +41,11 @@ function Update-Tools {
     This class is intended to be used with the Show-TUIMenu function which handles the GUI representation and interaction.
 #>
 
+  $ENV:ChocolateyInstall = if (Resolve-Path $XWAYSUSB ) {
+
+    Join-Path -Path $XWAYSUSB -ChildPath '*\chocolatey apps\chocolatey\bin' -Resolve
+  }
+
   class MenuItem {
     [string]$Name
     [scriptblock]$Action
@@ -58,7 +63,7 @@ function Update-Tools {
     }
   }
   $menuItem0 = [MenuItem]::new('All', { choco upgrade all --ignore-dependencies; winget install JanDeDobbeleer.OhMyPosh -s winget --force --accept-source-agreements --accept-package-agreements; Update-VisualStudio; Update-VSCode; Get-KapeAndTools; Get-GitPull; Update-PowerShell; Get-LatestSIV; winget source reset --disable-interactivity --force; winget source update --disable-interactivity; winget upgrade --all --force --accept-source-agreements --accept-package-agreements; DISM /Online /Cleanup-Image /RestoreHealth; sfc /scannow; Update-DotNetSDK })
-  $menuItem1 = [MenuItem]::new('UpgradeChocolateyAndTools', { $ENV:ChocolateyInstall = (Resolve-Path (Join-Path -Path $XWAYSUSB -ChildPath '*\chocolatey apps\chocolatey\bin')).Path; choco upgrade all --ignore-dependencies })
+  $menuItem1 = [MenuItem]::new('UpgradeChocolateyAndTools', { choco upgrade all --ignore-dependencies })
   $menuItem2 = [MenuItem]::new('InstallOhMyPosh', { winget install JanDeDobbeleer.OhMyPosh -s winget --force --accept-source-agreements --accept-package-agreements })
   $menuItem3 = [MenuItem]::new('UpdateVisualStudio', { Update-VisualStudio })
   $menuItem4 = [MenuItem]::new('UpdateVSCode', { Update-VSCode })
