6727082b69f35992b1470ba70808871b23dbe14d
dataman <michael@theablecoder.io>
2025-10-05 23:23:51 +1100

    refactor: Remove commented-out cmdlet installation and path management code for clarity

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,19 +19,7 @@ $ErrorActionPreference = 'continue'
 
 $XWAYSUSB = (Get-CimInstance -ClassName Win32_Volume -Filter "Label LIKE 'X-Ways%'").DriveLetter
 
-# $neededcmdlets = @('Install-Dependencies', 'Get-FileDownload', 'Invoke-AriaDownload', 'Get-LongName', 'Write-Logg', 'Get-Properties', 'Remove-PathEntry', 'Add-Paths')
-# $neededcmdlets | ForEach-Object {
-#   if (-not (Get-Command -Name $_ -ErrorAction SilentlyContinue)) {
-#     if (-not (Get-Command -Name 'Install-Cmdlet' -ErrorAction SilentlyContinue)) {
-#       $method = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/Donovoi/PowerShell-Profile/main/functions/Install-Cmdlet.ps1'
-#       $finalstring = [scriptblock]::Create($method.ToString() + "`nExport-ModuleMember -Function * -Alias *")
-#       New-Module -Name 'InstallCmdlet' -ScriptBlock $finalstring | Import-Module
-#     }
-#     Write-Verbose -Message "Importing cmdlet: $_"
-#     $Cmdletstoinvoke = Install-Cmdlet -RepositoryCmdlets $_
-#     $Cmdletstoinvoke | Import-Module -Force
-#   }
-# }
+
 # Define the profile path
 $powerShell7ProfilePath = [System.Environment]::GetFolderPath('MyDocuments') + '\PowerShell'
 
@@ -59,9 +47,6 @@ if (-not (Test-Path -Path $powerShell7ProfilePath)) {
   New-Item -Path $PROFILE
   Write-Logg -Message 'PowerShell 7 profile folders created successfully!' -Level Info
 }
-# # install and import modules needed for my profile
-# Install-Dependencies -InstallDefaultPSModules -NoNugetPackage
-
 
 # Variables for the commandline
 $vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
@@ -69,33 +54,6 @@ $vsInstaller = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vs_in
 
 
 Set-Alias -Name reboot -Value Get-NeededReboot -Option AllScope -Description 'Get-NeededReboot'
-
-# if (Test-Path -Path $XWAYSUSB -ErrorAction SilentlyContinue) {
-
-#   # We need to remove chocolatey from the path if it exists
-#   # Example usage for removing Chocolatey from both user and machine PATH
-#   Remove-PathEntry -pathToRemove 'C:\ProgramData\chocolatey\bin'
-
-#   # Function to add paths and persist changes
-#   # Define paths
-#   $chocolateyPath = (Resolve-Path (Join-Path -Path $XWAYSUSB -ChildPath '*\chocolatey apps\chocolatey\bin')).Path
-#   $nirsoftPath = (Resolve-Path (Join-Path -Path $XWAYSUSB -ChildPath '*\NirSoft\NirSoft\x64')).Path
-
-#   # Add paths and persist changes
-#   Add-Paths -chocolateyPath $chocolateyPath -nirsoftPath $nirsoftPath
-
-# }
-# else {
-#   $env:ChocolateyInstall = 'C:\ProgramData\chocolatey\bin'
-#   if (-not (Test-Path -Path $env:ChocolateyInstall) -or (-not (Get-Command -Name choco -ErrorAction SilentlyContinue))) {
-#     Write-Logg -Message 'Chocolatey is not installed. Installing now...' -level Warning
-#     Remove-Item -Path 'C:\ProgramData\chocolatey' -Recurse -Force -ErrorAction SilentlyContinue
-#     winget install Chocolatey.Chocolatey --source winget
-#     Write-Logg -Message 'Chocolatey installed successfully!' -level Info
-#   }
-#   $env:Path += "$env:ChocolateyInstall;$env:ChocolateyInstall\bin;$env:USERPROFILE\.cargo\bin;"
-
-# }
 
 # install oh-my-posh
 if (-not (Get-Command oh-my-posh -ErrorAction silentlycontinue)) {
@@ -109,17 +67,6 @@ if (-not (Get-Command oh-my-posh -ErrorAction silentlycontinue)) {
 if ($env:Path -notcontains '\oh-my-posh\bin') {
   $env:Path += ";$env:LOCALAPPDATA\Programs\oh-my-posh\bin"
 }
-
-# # Import the Chocolatey Profile
-# if (Test-Path -Path "$env:ChocolateyInstall\..\helpers\chocolateyProfile.psm1") {
-#   Import-Module "$env:ChocolateyInstall\..\helpers\chocolateyProfile.psm1"
-# }
-# else {
-#   Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1" -ErrorAction SilentlyContinue
-# }
-
-# # refresh the environment variables
-# Update-SessionEnvironment
 
 # Invoke an awesome sample of PSReadline bindings
 Invoke-SamplePSReadLineProfile
@@ -144,16 +91,4 @@ Set-PSReadLineOption -EditMode Windows
 # Crazy oh my posh random theme function
 Invoke-OhMyPoshRandomTheme
 
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-# if (Test-Path ($ChocolateyProfile)) {
-#   Import-Module "$ChocolateyProfile"
-# }
-
-# oh-my-posh init pwsh | Invoke-Expression
 # $trace.Top50SelfDuration
