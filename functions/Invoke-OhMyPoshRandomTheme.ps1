7fd6921631ba9894e4c20fcac44a7bad7d7aa353
dataman <michael@theablecoder.io>
2025-11-18 12:19:28 +1100

    Invoke-OhMyPoshRandomTheme: add installation check for Oh My Posh; automate installation via winget and update PATH if necessary

--- functions/Invoke-OhMyPoshRandomTheme.ps1
+++ functions/Invoke-OhMyPoshRandomTheme.ps1
@@ -2,6 +2,17 @@ function Invoke-OhMyPoshRandomTheme {
     [CmdletBinding()]
     param()
 
+    # ensure Oh My Posh is installed
+    if (-not (Get-Command -Name 'oh-my-posh' -ErrorAction SilentlyContinue)) {
+        # Install Oh My Posh via winget
+        Write-Host 'Oh My Posh not found. Installing via winget...'
+        winget install JanDeDobbeleer.OhMyPosh --source winget
+        # Add Oh My Posh to the PATH if not already present
+        if ($env:Path -notcontains "$env:LOCALAPPDATA\Programs\oh-my-posh\bin") {
+            $env:Path += ";$env:LOCALAPPDATA\Programs\oh-my-posh\bin"
+        }
+    }
+
     # Specify a default theme configuration to prevent the banner
     $null = oh-my-posh init pwsh | Invoke-Expression | Out-Null
 
